04c8402f54b8134a36158ceeca70b43531134255
alex <alexandrosrotsides@gmail.com>
2020-01-16 13:02:20 +0000

    added task to register one images

---
+++ register_query_image.py
@@ -0,0 +1,3 @@
+from image_registrator import register_image
+
+register_image("colmap_data/data/database.db", "colmap_data/data/current_query_image", "colmap_data/data/query_name.txt", "colmap_data/data/model/0", "colmap_data/data/new_model")
