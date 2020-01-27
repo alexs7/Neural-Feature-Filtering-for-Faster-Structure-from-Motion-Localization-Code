c40c4ba03affc65490ab42536b03ba50f391e36b
alex <alexandrosrotsidis@icloud.com>
2020-01-27 16:37:30 +0000

    wip after meeting in dc activ

--- evaluator.py
+++ evaluator.py
@@ -29,56 +29,38 @@ def show_projected_points(image_path, K, FP, points3D):
     cv2.waitKey(0)
 
 # get COLMAP pose the 3D points the image(s) are looking at
-# from: frame_1579029653869.jpg
-# to:   frame_1579029667636.jpg
+# from: frame_1579029492539.jpg
+# to:   frame_1579029499574.jpg
 # images between are:
-sequence = [ "1579029653869", "1579029654274", "1579029654999", "1579029655387", "1579029655774", "1579029656146",
-             "1579029656535", "1579029656939", "1579029657327", "1579029657732", "1579029658137", "1579029658508",
-             "1579029658895", "1579029659283", "1579029659688", "1579029660093", "1579029660482", "1579029660886",
-             "1579029661274", "1579029661662", "1579029662050", "1579029662438", "1579029662827", "1579029663235",
-             "1579029663620", "1579029664025", "1579029664430", "1579029664834", "1579029665241", "1579029665645",
-             "1579029666033", "1579029666437", "1579029666842", "1579029667248", "1579029667636"]
+sequence = ["1579029492539", "1579029492928", "1579029493315", "1579029493703", "1579029494092",
+            "1579029494497", "1579029494884", "1579029495272", "1579029495678",
+            "1579029496064", "1579029496437", "1579029496840", "1579029497228",
+            "1579029497616", "1579029498004", "1579029498411", "1579029498796",
+            "1579029499185", "1579029499574"]
 
-CP_start = get_query_image_global_pose("frame_" + sequence[0] + ".jpg")
+first_frame = sequence[0];
 
-LP_start = np.loadtxt("/Users/alex/Projects/EngDLocalProjects/Lego/fullpipeline/colmap_data/data/query_data/cameraPose_" + sequence[0] + ".txt")
-LP_start_dp_oriented = np.loadtxt("/Users/alex/Projects/EngDLocalProjects/Lego/fullpipeline/colmap_data/data/query_data/displayOrientedPose_" + sequence[0] + ".txt")
-# FP_start = LP_start.dot(np.linalg.inv(LP_start)).dot(CP_start) # Identity anyway..
-#
-# image_id_start = get_query_image_id("frame_"+first_frame+".jpg")
-# points3D_start = get_points3D(image_id_start)
-#
-# np.savetxt("points3D_start.txt", points3D_start)
-#
-# show_projected_points("/Users/alex/Projects/EngDLocalProjects/Lego/fullpipeline/colmap_data/data/current_query_image/frame_" + first_frame + ".jpg", K, FP_start, points3D_start)
+CP_start = get_query_image_global_pose("frame_"+first_frame+".jpg")
+
+LP_start = np.loadtxt("/Users/alex/Projects/EngDLocalProjects/Lego/fullpipeline/colmap_data/data/query_data/cameraPose_" + first_frame + ".txt")
+FP_start = LP_start.dot(np.linalg.inv(LP_start)).dot(CP_start) # Identity anyway..
+
+image_id_start = get_query_image_id("frame_"+first_frame+".jpg")
+points3D_start = get_points3D(image_id_start)
+
+np.savetxt("points3D_start.txt", points3D_start)
+
+show_projected_points("/Users/alex/Projects/EngDLocalProjects/Lego/fullpipeline/colmap_data/data/current_query_image/frame_" + first_frame + ".jpg", K, FP_start, points3D_start)
 
 colmap_poses = np.empty([4,4,len(sequence)])
-print("Getting ground truth poses from colmap..")
 for i in range(len(sequence)):
     colmap_pose = get_query_image_global_pose("frame_"+sequence[i]+".jpg")
     colmap_poses[:,:,i] = colmap_pose
 
 relative_pose = np.empty([4, 4])
-relative_pose_dp_oriented = np.empty([4, 4])
-print("Saving poses..")
 for i in range(1 , len(sequence)):
 
-    LP_dp_oriented = np.loadtxt("/Users/alex/Projects/EngDLocalProjects/Lego/fullpipeline/colmap_data/data/query_data/displayOrientedPose_" + sequence[i] + ".txt")
-
-    R1_dp_oriented = LP_start_dp_oriented[0:3,0:3]
-    t1_dp_oriented = LP_start_dp_oriented[0:3, 3]
-    R2_dp_oriented = LP_dp_oriented[0:3,0:3]
-    t2_dp_oriented = LP_dp_oriented[0:3,3]
-    R1to2_dp_oriented = np.linalg.inv(R2_dp_oriented).dot(R1_dp_oriented)
-    T1to2_dp_oriented = np.linalg.inv(R2_dp_oriented).dot((t1_dp_oriented - t2_dp_oriented))
-
-    relative_pose_dp_oriented[0:3, 0:3] = R1to2_dp_oriented
-    relative_pose_dp_oriented[0:3, 3] = T1to2_dp_oriented
-    relative_pose_dp_oriented[3, :] = [0, 0, 0, 1]
-
-    FP_dp_oriented = relative_pose_dp_oriented.dot(CP_start)
-
-    LP = np.loadtxt("/Users/alex/Projects/EngDLocalProjects/Lego/fullpipeline/colmap_data/data/query_data/cameraPose_" + sequence[i] + ".txt")
+    LP = np.loadtxt("/Users/alex/Projects/EngDLocalProjects/Lego/fullpipeline/colmap_data/data/query_data/cameraPose_"+sequence[i]+".txt")
 
     R1 = LP_start[0:3,0:3]
     t1 = LP_start[0:3, 3]
@@ -93,10 +75,9 @@ for i in range(1 , len(sequence)):
 
     FP = relative_pose.dot(CP_start)
 
-    # show_projected_points("/Users/alex/Projects/EngDLocalProjects/Lego/fullpipeline/colmap_data/data/current_query_image/frame_" + sequence[i] +".jpg", K, FP, points3D_start)
+    show_projected_points("/Users/alex/Projects/EngDLocalProjects/Lego/fullpipeline/colmap_data/data/current_query_image/frame_" + sequence[i] +".jpg", K, FP, points3D_start)
 
-    np.savetxt("final_poses/cameraPoses_" + sequence[i] + ".txt", FP)
-    np.savetxt("cameraPose_dp_oriented/cameraPose_dp_oriented" + sequence[i] + ".txt", FP_dp_oriented)
+    np.savetxt("final_poses/final_pose_" + sequence[i] + ".txt", FP)
     np.savetxt("colmap_poses/colmap_pose_" + sequence[i] + ".txt", colmap_poses[: , : , i])
 
 
--- view_poses.m
+++ view_poses.m
@@ -2,28 +2,123 @@ clear all;
 close all;
 
 points3D_start = importdata('points3D_start.txt');
-points3D_start = points3D_start(:,1:3)
+points3D_start = points3D_start(:,1:3);
 
 figure
-pcshow(points3D_start,'MarkerSize', 4);
+pcshow(points3D_start,'MarkerSize', 8);
+hold on
+% plot3(0,0,0,'g*');
+
+% set(gca,'color','w');
+xlabel('X');
+ylabel('Y');
+zlabel('Z');
 hold on
 
+minus_cameras = 0;
+
+gt_cam_loc = [];
 dinfo = dir('colmap_poses/*.txt');
-for i = 1 : length(dinfo)
+for i = 1 : length(dinfo) - minus_cameras
     pose = importdata(fullfile('colmap_poses/', dinfo(i).name));
     rotm = pose(1:3,1:3);
     tvec = pose(1:3,4);
+    
     camera_location = -inv(rotm) * tvec;
+    
+    gt_cam_loc = [gt_cam_loc ; camera_location'];
+    
+    % set to origin
+%     if(i == 1)
+%         diff = camera_location;
+%         camera_location = [0 0 0];
+%     else
+%         camera_location = camera_location - diff;
+%     end
+    
     plotCamera('Location', camera_location, 'Orientation', rotm, 'Size', 0.03, 'Color', [1, 0, 0]);
 end
 
 hold on
 
+fp_cam_loc = [];
 dinfo = dir('final_poses/*.txt');
-for i = 1 : length(dinfo)
+for i = 1 : length(dinfo) - minus_cameras
     pose = importdata(fullfile('final_poses/', dinfo(i).name));
-    rotm = pose(1:3,1:3);
-    tvec = pose(1:3,4);
+    rotm =  pose(1:3,1:3);
+    tvec =  pose(1:3,4);
+    
     camera_location = -inv(rotm) * tvec;
-    plotCamera('Location', camera_location, 'Orientation', rotm, 'Size', 0.03, 'Color', [0, 1, 0]);
+    
+    fp_cam_loc = [fp_cam_loc ; camera_location'];
+
+    % set to origin
+%     if(i == 1)
+%         diff = camera_location;
+%         camera_location = [0 0 0];
+%     else
+%         camera_location = camera_location - diff;
+%     end
+    
+%     plotCamera('Location', camera_location, 'Orientation', rotm, 'Size', 0.03, 'Color', [0, 1, 0], 'Opacity', 0.1);
 end
+
+gt_cam_loc = load('gt_cam_loc.mat');
+fp_cam_loc = load('fp_cam_loc.mat');
+[d,Z,transform] = procrustes(gt_cam_loc, fp_cam_loc);
+c = transform.c;
+T = transform.T;
+b = transform.b;
+
+hold on;
+%plot3(Z(:,1), Z(:,2), Z(:,3),'y*');
+
+% draw final poses
+dinfo = dir('final_poses/*.txt');
+for i = 1 : length(dinfo) - minus_cameras
+    pose = importdata(fullfile('final_poses/', dinfo(i).name));
+    rotm =  pose(1:3,1:3);
+    tvec =  pose(1:3,4);
+    
+    camera_location = -inv(rotm) * tvec;
+    
+    camera_location = b * camera_location' * T + c(1,:);
+    
+    % save new pose
+    t_new = -rotm * camera_location';
+    pose_new = [rotm t_new ; 0 0 0 1];
+    save(strcat(strcat('matlab_res/', dinfo(i).name ), '.mat'),'pose_new');
+    
+    % set to origin
+%     if(i == 1)
+%         diff = camera_location;
+%         camera_location = [0 0 0];
+%     else
+%         camera_location = camera_location - diff;
+%     end
+    
+    plotCamera('Location', camera_location, 'Orientation', rotm, 'Size', 0.03, 'Color', [0, 1, 0], 'Opacity', 0.1);
+    
+    
+end
+
+% hold on
+% 
+% dinfo = dir('cameraPose_dp_oriented/*.txt');
+% for i = 1 : length(dinfo) - minus_cameras
+%     pose = importdata(fullfile('cameraPose_dp_oriented/', dinfo(i).name));
+%     rotm =  pose(1:3,1:3);
+%     tvec =  pose(1:3,4);
+%     
+%     camera_location = -inv(rotm) * tvec;
+%     
+%     % set to origin
+%     if(i == 1)
+%         diff = camera_location;
+%         camera_location = [0 0 0];
+%     else
+%         camera_location = camera_location - diff;
+%     end
+%     
+%     plotCamera('Location', camera_location, 'Orientation', rotm, 'Size', 0.03, 'Color', [0, 0, 1], 'Opacity', 0.1);
+% end
