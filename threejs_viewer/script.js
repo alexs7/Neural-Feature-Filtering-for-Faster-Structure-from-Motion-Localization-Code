219c7dafdf1d1732e24f28ad158e8a31bb417bf3
alex <alexandrosrotsidis@icloud.com>
2020-02-17 17:20:34 +0000

    finished debug threejs viewer

--- threejs_viewer/script.js
+++ threejs_viewer/script.js
@@ -1,11 +1,10 @@
 const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
-const hostname = '127.0.0.1';
-const port = 3000;
 
 //3D Objects
 var cone;
+var anchor;
 
 window.onload = function() {
 
@@ -15,11 +14,38 @@ window.onload = function() {
     app.use(bodyParser.text());
 
     app.post('/', (req, res) => {
-        // console.log('Got body:', req.body);
+        console.log('Got body:', req.body);
+
+        tx = parseFloat(req.body.split(',')[0]);
+        ty = parseFloat(req.body.split(',')[1]);
+        tz = parseFloat(req.body.split(',')[2]);
+        qx = parseFloat(req.body.split(',')[3]);
+        qy = parseFloat(req.body.split(',')[4]);
+        qz = parseFloat(req.body.split(',')[5]);
+        qw = parseFloat(req.body.split(',')[6]);
+
+        cone.position.x = tx;
+        cone.position.y = ty;
+        cone.position.z = tz;
+
+        var quaternion = new THREE.Quaternion();
+        quaternion.fromArray([qx,qy,qz,qw]);
+        quaternion.normalize();
+        cone.setRotationFromQuaternion(quaternion);
+
+        // cone.rotation.x += Math.PI/2;
+
+        anchor_tx = parseFloat(req.body.split(',')[7]);
+        anchor_ty = parseFloat(req.body.split(',')[8]);
+        anchor_tz = parseFloat(req.body.split(',')[9]);
+
+        anchor.position.x = anchor_tx;
+        anchor.position.y = anchor_ty;
+        anchor.position.z = anchor_tz;
 
         res.sendStatus(200);
     });
-    
+
     app.listen(3000, () => console.log(`Started server at http://localhost:3000!`));
 
     var scene = new THREE.Scene();
@@ -29,8 +55,8 @@ window.onload = function() {
     renderer.setSize( window.innerWidth, window.innerHeight );
     document.body.appendChild( renderer.domElement );
 
-    var size = 100;
-    var divisions = 100;
+    var size = 10;
+    var divisions = 10;
 
     var gridHelper = new THREE.GridHelper( size, divisions );
     scene.add( gridHelper );
@@ -38,19 +64,41 @@ window.onload = function() {
     var axesHelper = new THREE.AxesHelper( 5 );
     scene.add( axesHelper );
 
-    var geometry = new THREE.ConeGeometry( 1, 1, 4 );
-    var wireframe = new THREE.WireframeGeometry( geometry );
-    cone = new THREE.LineSegments( wireframe );
-    cone.material.color.setHex( 0xff0000 );
-    cone.rotation.x = Math.PI/2;
-    cone.rotation.y = Math.PI/4;
-    cone.position.z = 0.5;
+    // var geometry = new THREE.ConeGeometry( 1, 1, 4 );
+
+    var geometry = new THREE.Geometry();
+    geometry.vertices.push(
+      new THREE.Vector3(1, 1, 0),  // 0
+      new THREE.Vector3(-1, 1, 0),  // 1
+      new THREE.Vector3(-1, -1, 0),  // 2
+      new THREE.Vector3(1, -1, 0),  // 3
+      new THREE.Vector3(0, 0, 2),  // 4
+    );
+
+    var material =  new THREE.PointsMaterial( { color: 0xff0000, size: 0.03 } );
+    cone = new THREE.Points( geometry, material );
+    cone.position.z = 0.25;
+
+    var axesHelperCone = new THREE.AxesHelper( 5 );
+    cone.add(axesHelperCone);
     scene.add( cone );
+
+    var geometry = new THREE.SphereGeometry( 1, 32, 32 );
+    var material = new THREE.MeshPhongMaterial( {color: 0xffff00} );
+    anchor = new THREE.Mesh( geometry, material );
+    scene.add( anchor );
+
+    var light = new THREE.DirectionalLight( 0xffffff );
+    light.position.set( 50, 50, 50 );
+    scene.add( light );
+
+    cone.scale.set(0.1,0.1,0.1);
+    anchor.scale.set(0.05,0.05,0.05);
 
     var controls = new THREE.TrackballControls(camera, renderer.domElement);
 
     //controls.update() must be called after any manual changes to the camera's transform
-    camera.position.set( 5, 5, 5 );
+    camera.position.set( 1, 1, 1 );
     controls.update();
 
     function animate() {
