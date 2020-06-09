6ca9a68158b0a09de0e0bd474645e829b1f8451c
alex <alexandrosrotsidis@icloud.com>
2020-06-09 13:28:08 +0100

    added prosac translated version from c

--- ransac.py
+++ ransac.py
@@ -1,7 +1,9 @@
 # This is vanilla RANSAC Implementation and Modified one
+import random
 import numpy as np
 import cv2
 import time
+import scipy.special
 
 # intrinsics matrix
 K = np.loadtxt(
@@ -33,7 +35,7 @@ def run_ransac(matches_for_image):
     p = 0.99 # this is a typical value
     # number of iterations (http://www.cse.psu.edu/~rtc12/CSE486/lecture15.pdf and https://youtu.be/5E5n7fhLHEM?list=PLTBdjV_4f-EKeki5ps2WHqJqyQvxls4ha&t=428)
     # also reddit post https://www.reddit.com/r/computervision/comments/gikj1s/can_somebody_help_me_understand_ransac/
-    no_iterations = 1000  # can set this to whatever you want to start with
+    no_iterations = 20000  # can set this to whatever you want to start with
     k = 0
     distCoeffs = np.zeros((5, 1))  # assume zero for now
     threshold = 8.0 # same as opencv
@@ -41,6 +43,7 @@ def run_ransac(matches_for_image):
     best_model = {}
     elapsed_time_total_for_random_sampling = 0
     while k < no_iterations:
+
         inliers = []
         # pick 4 random matches (assume they are inliers)
         start = time.time()
@@ -77,7 +80,7 @@ def run_ransac(matches_for_image):
         # store best model so far
         if(inlers_no > max):
             best_model['Rt'] = Rt
-            best_model['inlers_no'] = inlers_no
+            best_model['inlers_no'] = inlers_no #TODO: Do you need this ?
             max = inlers_no
             e = outliers_no / len(matches_for_image)
             N = np.log(1 - p) / np.log(1 - np.power((1 - e), s))
@@ -85,6 +88,7 @@ def run_ransac(matches_for_image):
             no_iterations = N
             if(k > N): # this is saying if the max number of iterations you should have run is N, but you already did k > N then no point continuing
                 return (inlers_no, outliers_no, k, best_model, elapsed_time_total_for_random_sampling)
+
         k = k + 1
 
     return (inlers_no, outliers_no, k, best_model, elapsed_time_total_for_random_sampling)
@@ -154,6 +158,221 @@ def run_ransac_modified(matches_for_image, distribution):
         k = k + 1
     return (inlers_no, outliers_no, k, best_model, elapsed_time_total_for_random_sampling)
 
-def prosac(matches_for_image):
-    print()
+def prosac_new(sorted_matches):
+    # TODO: move this distCoeffs out!
+    distCoeffs = np.zeros((5, 1))  # assume zero for now
 
+    CORRESPONDENCES = sorted_matches.shape[0]
+    SAMPLE_SIZE = 4
+    MAX_OUTLIERS_PROPORTION = 0.8
+    P_GOOD_SAMPLE = 0.99
+    TEST_NB_OF_DRAWS = 60000
+    TEST_INLIERS_RATIO = 0.5
+    BETA = 0.1
+    ETA0 = 0.05
+
+    def niter_RANSAC(p, epsilon, s, Nmax):
+        if(Nmax == -1):
+            Nmax = np.iinfo(np.int32).max
+        if(not (Nmax >= 1)):
+            print("C++ Assertion failed - 1")
+        if(epsilon <= 0):
+            return 1
+        logarg = - np.exp(s * np.log(1 - epsilon))
+        logval = np.log(1 + logarg)
+        N = np.log(1 - p) / logval
+        if(logval < 0 and N < Nmax):
+            return np.ceil(N)
+
+        return Nmax
+
+    def Imin(m, n, beta):
+        mu = n*beta
+        sigma = np.sqrt(n * beta * (1 - beta))
+        return  np.ceil(m + mu + sigma * np.sqrt(2.706))
+
+    N = CORRESPONDENCES
+    m = SAMPLE_SIZE
+    T_N = niter_RANSAC(P_GOOD_SAMPLE,MAX_OUTLIERS_PROPORTION,SAMPLE_SIZE,-1)
+    beta = BETA
+    I_N_min = (1 - MAX_OUTLIERS_PROPORTION)*N
+    logeta0 = np.log(ETA0)
+
+    # print("PROSAC sampling test\n")
+    # print("number of correspondences (N):{0}\n".format(N))
+    # print("sample size (m):{0}\n".format(m))
+    # print("showing the first {0} draws from PROSAC\n".format(TEST_NB_OF_DRAWS))
+
+    n_star = N
+    I_n_star = 0
+    I_N_best = 0
+    t = 0
+    n = m
+    T_n = T_N
+
+    for i in range(m):
+        T_n = T_n * (n - i) / (N - i)
+
+    T_n_prime = 1
+    k_n_star = T_N
+
+    best_model = {}
+    while (((I_N_best < I_N_min) or t <= k_n_star) and t < T_N and t <= TEST_NB_OF_DRAWS):
+        model = {}
+        inliers = []
+        t = t + 1
+        # print("Iteration t=%d, " % t)
+
+        if ((t > T_n_prime) and (n < n_star)):
+            T_nplus1 = (T_n * (n+1)) / (n+1-m)
+            n = n + 1
+            T_n_prime = T_n_prime + np.ceil(T_nplus1 - T_n)
+            T_n = T_nplus1
+
+        if (t > T_n_prime):
+            pts_idx = np.random.choice(n, m, replace=False)
+        else:
+            pts_idx = np.append(np.random.choice(n-1, m-1, replace=False), n-1)
+
+        sample = sorted_matches[pts_idx]
+
+        # 3. Model parameter estimation
+        obj_points = sample[:, 2:5]
+        img_points = sample[:, 0:2]
+
+        img_points = np.ascontiguousarray(img_points[:, :2]).reshape((img_points.shape[0], 1, 2))  # this is required for SOLVEPNP_P3P
+        retval, rvec, tvec = cv2.solvePnP(obj_points.astype(np.float32), img_points.astype(np.float32), K, distCoeffs, flags=cv2.SOLVEPNP_P3P)
+        rotm = cv2.Rodrigues(rvec)[0]
+        Rt = np.r_[(np.c_[rotm, tvec]), [np.array([0, 0, 0, 1])]]
+        model['Rt'] = Rt
+
+        # 4. Model verification
+        for i in range(len(sorted_matches)):  # run against all the other matches (all of them doesn't matter here)
+            obj_point = sorted_matches[i, 2:5]
+            img_point_gt = sorted_matches[i, 0:2]
+            obj_point = np.r_[obj_point, 1]  # make homogeneous
+            img_point_est = K.dot(Rt.dot(obj_point.transpose())[0:3])
+            img_point_est = img_point_est / img_point_est[2]  # divide by last coordinate
+            dist = np.linalg.norm(img_point_gt - img_point_est[0:2])
+            if (dist < 8.0):
+                inliers.append(sorted_matches[i])
+
+        I_N = len(inliers)
+        # print("found {0} inliers!\n".format(I_N))
+
+        if(I_N > I_N_best):
+            I_N_best = I_N
+            n_best = N
+            I_n_best = I_N
+            best_model = model
+
+            if (I_n_best * n_star > I_n_star * n_best):
+                if(not (n_best >= I_n_best)):
+                    print("C++ Assertion failed - 2")
+                n_star = n_best
+                I_n_star = I_n_best
+                k_n_star = niter_RANSAC(1 - ETA0, 1 - I_n_star / n_star, m, T_N)
+
+    # print("PROSAC finished, reason:\n");
+    # if(t > TEST_NB_OF_DRAWS):
+    #     print("t={0} > max_t={1} (k_n_star={2}, T_N={3})\n".format(t, TEST_NB_OF_DRAWS, k_n_star, T_N))
+    # elif(t > T_N):
+    #     print("t={0} > T_N={1} (k_n_star={2})\n".format(t, T_N, k_n_star))
+    # elif(t > k_n_star):
+    #     print("t={0} > k_n_star={1} (T_N={2})\n".format(t ,k_n_star, T_N))
+
+    return I_N, len(sorted_matches) - I_N, t, best_model
+
+# def prosac(sorted_matches, beta=0.5, phi=0.05, eta=0.05):
+#     # TODO: move this distCoeffs out!
+#     distCoeffs = np.zeros((5, 1))  # assume zero for now
+#
+#     # https: // github.com / willGuimont / PROSAC / blob / master / random_consensus.py
+#     T_N = 20000
+#     N = sorted_matches.shape[0]
+#     m = 4
+#     t = 0
+#     n = m
+#     n_star = N
+#     Tn = T_N
+#     for i in range(m):
+#         Tn = Tn * (n-i) / (N-i)
+#
+#     Tn_prime = 1 #as defined above equation (4)
+#
+#     model = {}
+#
+#     while t < T_N:
+#         inliers = []
+#         # 1. Choice of the hypothesis generation set
+#         t = t + 1
+#         if t > Tn_prime and n < n_star:
+#             Tn_1 = (Tn * (n + 1)) / (n + 1 - m)
+#             n = n + 1
+#             if(n == 26) : breakpoint()
+#             Tn_prime = Tn_prime + np.ceil(Tn_1 - Tn) #make Tn integer
+#             Tn = Tn_1
+#
+#         # 2. Semi-random sample M of size m
+#         if t > Tn_prime:
+#             pts_idx = random.sample(range(n), m)
+#         else:
+#             pts_idx = [n] + random.sample(range(n - 1), m - 1)
+#
+#         print(pts_idx)
+#         # if (26 in pts_idx):
+#         #     print("n: " + str(n))
+#         #     breakpoint()
+#
+#         sample = sorted_matches[pts_idx]
+#
+#         # 3. Model parameter estimation
+#         obj_points = sample[:, 2:5]
+#         img_points = sample[:, 0:2]
+#
+#         img_points = np.ascontiguousarray(img_points[:, :2]).reshape((img_points.shape[0], 1, 2)) # this is required for SOLVEPNP_P3P
+#         retval, rvec, tvec = cv2.solvePnP(obj_points.astype(np.float32), img_points.astype(np.float32), K, distCoeffs, flags=cv2.SOLVEPNP_P3P)
+#         rotm = cv2.Rodrigues(rvec)[0]
+#         Rt = np.r_[(np.c_[rotm, tvec]), [np.array([0, 0, 0, 1])]]
+#         model['Rt'] = Rt
+#
+#         # 4. Model verification
+#         for i in range(len(sorted_matches)): # run against all the other matches (all of them doesn't matter here)
+#             obj_point = sorted_matches[i, 2:5]
+#             img_point_gt = sorted_matches[i, 0:2]
+#             obj_point = np.r_[obj_point, 1]  # make homogeneous
+#             img_point_est = K.dot(Rt.dot(obj_point.transpose())[0:3])
+#             img_point_est = img_point_est / img_point_est[2]  # divide by last coordinate
+#             dist = np.linalg.norm(img_point_gt - img_point_est[0:2])
+#             if (dist < 8.0):
+#                 inliers.append(sorted_matches[i])
+#
+#         if(len(inliers) >= 6):
+#             print("Found " + str(len(inliers)) + "/" + str(len(sorted_matches)) + " inlers")
+#         num_inliers = len(inliers)
+#         outliers_no = len(sorted_matches) - num_inliers
+#
+#         # 4.1 Non random solution
+#         def P(i):
+#             return (beta ** (i - m)) * ((1 - beta) ** (n - i + m)) * scipy.special.binom(n - m, i - m)
+#
+#         def Imin_value(j):
+#             return sum(P(i) for i in range(j, n + 1))
+#
+#         imin = 0
+#         for j in range(m, N - m):
+#             value = Imin_value(j)
+#             if value < phi:
+#                 imin = j
+#                 break
+#
+#         non_random = num_inliers > imin
+#
+#         # 4.2 Maximality
+#         Pin = scipy.special.binom(num_inliers, m) / scipy.special.binom(n_star, m)
+#         maximality = (1 - Pin) ** t <= eta
+#
+#         if non_random and maximality:
+#             break
+#
+#     return (num_inliers, outliers_no, t, model)
--- ransac_comparison.py
+++ ransac_comparison.py
@@ -2,7 +2,7 @@ import numpy as np
 
 from point3D_loader import read_points3d_default
 from query_image import load_images_from_text_file, read_images_binary
-from ransac import run_ransac, run_ransac_modified
+from ransac import run_ransac, run_ransac_modified, prosac_new
 import time
 
 # get the "sub_distributions" for each matches set for each image
@@ -125,35 +125,146 @@ def run_ransac_comparison(features_no, exponential_decay_value, weighted=False):
 
     print("Done!")
 
-def run_prosac_comparison(features_no, exponential_decay_value):
+def run_prosac_comparison(features_no, exponential_decay_value, weighted=False):
 
     complete_model_points3D_path = "/Users/alex/Projects/EngDLocalProjects/LEGO/fullpipeline/colmap_data/data/multiple_localised_models/"+features_no+"/points3D.bin"
     points3D = read_points3d_default(complete_model_points3D_path)
-    points_values = np.loadtxt("/Users/alex/Projects/EngDLocalProjects/LEGO/fullpipeline/colmap_data/data/visibility_matrices/"+features_no+"/heatmap_matrix_avg_points_values_"+str(exponential_decay_value)+".txt")
+    points_heatmap_values = np.loadtxt("/Users/alex/Projects/EngDLocalProjects/LEGO/fullpipeline/colmap_data/data/visibility_matrices/"+features_no+"/heatmap_matrix_avg_points_values_"+str(exponential_decay_value)+".txt")
 
-    # create index and point value relationship
-    # remember points3D and points_values have the same order
+    # create points id and index relationship
     point3D_index = 0
     points3D_indexing = {}
     for key, value in points3D.items():
-        points3D_indexing[point3D_index] = points_values[point3D_index]
+        points3D_indexing[point3D_index] = value.id
         point3D_index = point3D_index + 1
 
-    # sorted in desceding, values for prosac
-    points3D_indexing = {k: v for k, v in sorted(points3D_indexing.items(), key=lambda item: -item[1])}
-    points_values_desc = list(points3D_indexing.values())
+    print("-- Doing features_no " + features_no + " --")
 
-    breakpoint()
+    # load localised images names (including base ones)
+    localised_images = load_images_from_text_file("/Users/alex/Projects/EngDLocalProjects/LEGO/fullpipeline/colmap_data/data/images_localised_and_not_localised/" + features_no + "/images_localised.txt")
+    # of course base images will be localised..
+    base_images = load_images_from_text_file("/Users/alex/Projects/EngDLocalProjects/LEGO/fullpipeline/colmap_data/data/base_images.txt")
+    # Now, get localised images from the query images only. Not the base images.
+    localised_query_images_only = []
+    for image in localised_images:
+        if(image not in base_images):
+            localised_query_images_only.append(image)
 
+    # Question: why not using matches_base ?!?!! and comparing to that ?
+    # Answer: and compare what ? ransac will take less time with matches_base given the smaller number of all matches,
+    # plus the get_sub_distribution might not make sense here as for base there are no future sessions yet.
+    if(weighted): #if statement here is self explanatory
+        matches_all = np.load("/Users/alex/Projects/EngDLocalProjects/LEGO/fullpipeline/colmap_data/data/feature_matching/"+features_no+"/matches_all_weighted.npy")
+    else:
+        matches_all = np.load("/Users/alex/Projects/EngDLocalProjects/LEGO/fullpipeline/colmap_data/data/feature_matching/" + features_no + "/matches_all.npy")
 
+    #distribution; row vector, same size as 3D points
+    points3D_avg_heatmap_vals = np.loadtxt("/Users/alex/Projects/EngDLocalProjects/LEGO/fullpipeline/colmap_data/data/visibility_matrices/"+features_no+"/heatmap_matrix_avg_points_values_" + str(exponential_decay_value) + ".txt")
+    points3D_avg_heatmap_vals = points3D_avg_heatmap_vals.reshape([1, points3D_avg_heatmap_vals.shape[0]])
+
+    # print("Getting sub_distributions..")
+    # distributions = {}
+    # for image in localised_query_images_only:
+    #     matches_for_image = matches_all.item()[image]
+    #     breakpoint()
+    #     distributions[image] = get_sub_distribution(matches_for_image, points3D_avg_heatmap_vals)
+
+    print("Running RANSAC/PROSAC.. for exponential decay of value: " + str(exponential_decay_value))
+
+    #  this will hold inliers_no, ouliers_no, iterations, time for each image
+    vanilla_ransac_data = np.empty([0, 4])
+    vanilla_ransac_images_poses = {}
+
+    prosac_data = np.empty([0, 4])
+    prosac_images_poses = {}
+
+    for i in range(len(localised_query_images_only)):
+        image = localised_query_images_only[i]
+        matches_for_image = matches_all.item()[image]
+        print("Doing image " + str(i+1) + "/" + str(len(localised_query_images_only)) + ", " + image , end="\r")
+
+        if(len(matches_for_image) >= 4):
+            # vanilla
+            start = time.time()
+            inliers_no, ouliers_no, iterations, best_model, _ = run_ransac(matches_for_image)
+            end  = time.time()
+            elapsed_time = end - start
+
+            vanilla_ransac_images_poses[image] = best_model
+            vanilla_ransac_data = np.r_[vanilla_ransac_data, np.array([inliers_no, ouliers_no, iterations, elapsed_time]).reshape([1,4])]
+
+            # get sorted image matches
+            indices = matches_for_image[:, 5].astype(int)
+            values_for_matches = np.array(points_heatmap_values)[indices]
+            matches_for_image[:,5] = values_for_matches
+            # sorted_indices
+            sorted_indices = np.argsort(matches_for_image[:, 5])
+            # in descending order
+            sorted_matches = matches_for_image[sorted_indices[::-1]]
+            # sorted_matches[:, 5] = sorted_matches[:, 5] / sorted_matches[:, 5].sum()
+
+            # prosac
+            start = time.time()
+            inliers_no, ouliers_no, iterations, best_model = prosac_new(sorted_matches)
+            end = time.time()
+            elapsed_time = end - start
+
+            prosac_images_poses[image] = best_model
+            prosac_data = np.r_[prosac_data, np.array([inliers_no, ouliers_no, iterations, elapsed_time]).reshape([1, 4])]
+        else:
+            print(image + " has less than 4 matches..")
+            
+    # NOTE: folders .../RANSAC_results/"+features_no+"/... where created manually..
+    if(weighted):
+        np.save("/Users/alex/Projects/EngDLocalProjects/LEGO/fullpipeline/colmap_data/data/RANSAC_results/"+features_no+"/vanilla_ransac_images_pose_" + str(exponential_decay_value) + "_weighted.npy", vanilla_ransac_images_poses)
+        np.save("/Users/alex/Projects/EngDLocalProjects/LEGO/fullpipeline/colmap_data/data/RANSAC_results/"+features_no+"/vanilla_ransac_data_" + str(exponential_decay_value) + "_weighted.npy", vanilla_ransac_data)
+
+        np.save("/Users/alex/Projects/EngDLocalProjects/LEGO/fullpipeline/colmap_data/data/RANSAC_results/"+features_no+"/modified_ransac_images_pose_" + str(exponential_decay_value) + "_weighted.npy", prosac_images_poses)
+        np.save("/Users/alex/Projects/EngDLocalProjects/LEGO/fullpipeline/colmap_data/data/RANSAC_results/"+features_no+"/modified_ransac_data_" + str(exponential_decay_value) + "_weighted.npy", prosac_data)
+
+        print("\n")
+        print("Weighted Matches Results for exponential_decay_value " + str(exponential_decay_value/10) + ":")
+        print("Vanillia RANSAC")
+        print("     Average Inliers: " + str(np.mean(vanilla_ransac_data[:,0])))
+        print("     Average Outliers: " + str(np.mean(vanilla_ransac_data[:,1])))
+        print("     Average Iterations: " + str(np.mean(vanilla_ransac_data[:,2])))
+        print("     Average Time (s): " + str(np.mean(vanilla_ransac_data[:,3])))
+        print("PROSAC")
+        print("     Average Inliers: " + str(np.mean(prosac_data[:, 0])))
+        print("     Average Outliers: " + str(np.mean(prosac_data[:, 1])))
+        print("     Average Iterations: " + str(np.mean(prosac_data[:, 2])))
+        print("     Average Time (s): " + str(np.mean(prosac_data[:, 3])))
+        print("<---->")
+    else:
+        np.save("/Users/alex/Projects/EngDLocalProjects/LEGO/fullpipeline/colmap_data/data/RANSAC_results/"+features_no+"/vanilla_ransac_images_pose_" + str(exponential_decay_value) + ".npy", vanilla_ransac_images_poses)
+        np.save("/Users/alex/Projects/EngDLocalProjects/LEGO/fullpipeline/colmap_data/data/RANSAC_results/"+features_no+"/vanilla_ransac_data_" + str(exponential_decay_value) + ".npy", vanilla_ransac_data)
+
+        np.save("/Users/alex/Projects/EngDLocalProjects/LEGO/fullpipeline/colmap_data/data/RANSAC_results/"+features_no+"/modified_ransac_images_pose_" + str(exponential_decay_value) + ".npy", prosac_images_poses)
+        np.save("/Users/alex/Projects/EngDLocalProjects/LEGO/fullpipeline/colmap_data/data/RANSAC_results/"+features_no+"/modified_ransac_data_" + str(exponential_decay_value) + ".npy", prosac_data)
+
+        print("\n")
+        print("Non-Weighted Matches Results for exponential_decay_value " + str(exponential_decay_value/10) + ":")
+        print("Vanillia RANSAC")
+        print("     Average Inliers: " + str(np.mean(vanilla_ransac_data[:,0])))
+        print("     Average Outliers: " + str(np.mean(vanilla_ransac_data[:,1])))
+        print("     Average Iterations: " + str(np.mean(vanilla_ransac_data[:,2])))
+        print("     Average Time (s): " + str(np.mean(vanilla_ransac_data[:,3])))
+        print("PROSAC")
+        print("     Average Inliers: " + str(np.mean(prosac_data[:, 0])))
+        print("     Average Outliers: " + str(np.mean(prosac_data[:, 1])))
+        print("     Average Iterations: " + str(np.mean(prosac_data[:, 2])))
+        print("     Average Time (s): " + str(np.mean(prosac_data[:, 3])))
+        print("<---->")
+
+    print("Done!")
 
 # colmap_features_no can be "2k", "1k", "0.5k", "0.25k"
 # exponential_decay can be any of 0.1 to 0.9
-print("Running RANSAC comparison against un-weighted matches")
+# print("Running RANSAC comparison against un-weighted matches")
 # run_ransac_comparison("1k", 0.5)
-print("Running RANSAC comparison against weighted matches")
+# print("Running RANSAC comparison against weighted matches")
 # run_ransac_comparison("1k", 0.5, True)
-# print("Running PROSAC comparison against weighted matches")
-# run_prosac_comparison("1k", 0.5)
+print("Running PROSAC comparison against weighted matches")
+run_prosac_comparison("1k", 0.5)
 
 
