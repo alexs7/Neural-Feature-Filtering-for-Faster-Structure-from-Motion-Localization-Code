76a8b2832af060005d22ab0977f964f0dfa0d4e6
alex <alexandrosrotsidis@icloud.com>
2020-06-12 09:10:45 +0100

    minor fixes pose evaluator

--- plot_dists.m
+++ plot_dists.m
@@ -1,9 +1,9 @@
 close all;
 
-dist = importdata('/Users/alex/Projects/EngDLocalProjects/LEGO/fullpipeline/colmap_data/data/pose_evaluator/vanilla_ransac_results_t_1k_0.5_weighted.txt');
+dist = importdata('/Users/alex/Projects/EngDLocalProjects/LEGO/fullpipeline/colmap_data/data/pose_evaluator/modified_results_t_1k_0.5.txt');
 figure
 bar(dist);
 
-dist = importdata('/Users/alex/Projects/EngDLocalProjects/LEGO/fullpipeline/colmap_data/data/pose_evaluator/modified_ransac_results_t_1k_0.5_weighted.txt');
+dist = importdata('/Users/alex/Projects/EngDLocalProjects/LEGO/fullpipeline/colmap_data/data/pose_evaluator/vanillia_results_t_1k_0.5.txt');
 figure
 bar(dist);
