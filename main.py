8b2cb9e67134c7e10106dfe83676b5b3474985f6
alex <alexandrosrotsidis@icloud.com>
2020-06-18 20:11:16 +0100

    refactored params in main

--- main.py
+++ main.py
@@ -6,29 +6,27 @@ from parameters import Parameters
 from point3D_loader import read_points3d_default
 import numpy as np
 
+from query_image import get_images_names_bin
 from ransac_comparison import run_comparison
 from ransac_prosac import ransac, prosac
 
 features_no = "1k" # colmap_features_no can be "2k", "1k", "0.5k", "0.25k"
 exponential_decay_value = 0.5 # exponential_decay can be any of 0.1 to 0.9
+
 db_path = "/Users/alex/Projects/EngDLocalProjects/LEGO/fullpipeline/colmap_data/data/multiple_localised_models/"+features_no+"/database.db"
-# images_path is the file of list of images to get 2D-3D matches from
-# NOTE: Here you can use the "colmap_data/data/query_name.txt" if you want to exlude base images..
-images_path = "/Users/alex/Projects/EngDLocalProjects/LEGO/fullpipeline/colmap_data/data/images_localised_and_not_localised/" + features_no + "/images_localised.txt"
-base_images_path = "/Users/alex/Projects/EngDLocalProjects/LEGO/fullpipeline/colmap_data/data/base_images.txt"
+
+base_model_images_path = "/Users/alex/Projects/EngDLocalProjects/LEGO/fullpipeline/colmap_data/all_data_and_models/coop_local/2020-03-28/coop_local/reconstruction/model/0/images.bin"
+complete_model_images_path = "/Users/alex/Projects/EngDLocalProjects/LEGO/fullpipeline/colmap_data/data/multiple_localised_models/" + features_no + "/images.bin"
+
+base_images_names = get_images_names_bin(base_model_images_path)
+all_images_names = get_images_names_bin(complete_model_images_path) #all = query + base images
+
 train_descriptors_all_path = "/Users/alex/Projects/EngDLocalProjects/LEGO/fullpipeline/colmap_data/data/descriptors_avg/"+features_no+"/points_mean_descs_all.npy"
 train_descriptors_base_path = "/Users/alex/Projects/EngDLocalProjects/LEGO/fullpipeline/colmap_data/data/descriptors_avg/"+features_no+"/points_mean_descs_base.npy"
 
 # by "complete model" I mean all the frames from future sessions localised in the base model (28/03)
 complete_model_points3D_path = "/Users/alex/Projects/EngDLocalProjects/LEGO/fullpipeline/colmap_data/data/multiple_localised_models/" + features_no + "/points3D.bin"
 points3D = read_points3d_default(complete_model_points3D_path)  # base model's 3D points (same length as complete as we do not add points when localising new points, but different image_ds for each point)
-
-# create points id and index relationship
-point3D_index = 0
-points3D_indexing = {}
-for key, value in points3D.items():
-    points3D_indexing[point3D_index] = value.id
-    point3D_index = point3D_index + 1
 
 # define matcher
 matching_algo = FeatureMatcherTypes.FLANN  # or FeatureMatcherTypes.BF
@@ -51,14 +49,14 @@ modified_data_path_save_info = "/Users/alex/Projects/EngDLocalProjects/LEGO/full
 
 # Run Code here
 # 1: Feature matching
-# matches = feature_matcher_wrapper(points3D_avg_heatmap_vals, db_path, images_path, train_descriptors_all_path, points3D, points3D_indexing, matcher)
+matches = feature_matcher_wrapper(points3D_avg_heatmap_vals, db_path, all_images_names, train_descriptors_all_path, points3D, matcher)
 
 # print("Saving matches...")
 # save the 2D-3D matches
-# np.save(matches_path, matches)
+np.save(matches_path, matches)
 
 # 2: RANSAC Comparison
-vanilla_images_poses, vanilla_data, modified_images_poses, modified_data = run_comparison(0.5, ransac, prosac, matches_path, images_path, base_images_path)
+vanilla_images_poses, vanilla_data, modified_images_poses, modified_data = run_comparison(0.5, ransac, prosac, matches_path, all_images_names)
 
 print("Saving RANSAC data...")
 # NOTE: folders .../RANSAC_results/"+features_no+"/... where created manually..
