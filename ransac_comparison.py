2de876e58c068c40e6b77d86148d06d5dac7b2d9
alex <alexandrosrotsidis@icloud.com>
2020-06-24 12:11:15 +0100

    refactored ransac comparison

--- ransac_comparison.py
+++ ransac_comparison.py
@@ -9,9 +9,30 @@ def get_sub_distribution(matches_for_image, distribution):
     indices = indices.astype(int)
     sub_distribution = distribution[0, indices]
     sub_distribution = sub_distribution / np.sum(sub_distribution)
+    sub_distribution = sub_distribution.reshape([sub_distribution.shape[0], 1])
     return sub_distribution
 
-def run_comparison(exponential_decay_value, ransac, run_ransac_modified, prosac, matches_path, test_images, points3D_avg_heatmap_vals):
+# functions for PROSAC's score list
+def enhanced_sort_matches(matches):
+    lowes_distances = matches[:, 6] / matches[:, 6].sum()
+    heatmap_vals = matches[:, 7] / matches[:, 7].sum()
+    score_list = lowes_distances * heatmap_vals
+    # sorted_indices
+    sorted_indices = np.argsort(score_list)
+    # in descending order
+    sorted_matches = matches[sorted_indices[::-1]]
+    return sorted_matches
+
+def plain_sort_matches(matches):
+    lowes_distances = matches[:, 6] / matches[:, 6].sum()
+    score_list = lowes_distances
+    # sorted_indices
+    sorted_indices = np.argsort(score_list)
+    # in descending order
+    sorted_matches = matches[sorted_indices[::-1]]
+    return sorted_matches
+
+def run_comparison(func, matches_path, test_images, points3D_avg_heatmap_vals = np.array([]) , sort_matches_func = None):
 
     # Question: why not using matches_base ?!?!! and comparing to that ?
     # Answer: and compare what ? ransac will take less time with matches_base given the smaller number of all matches,
@@ -19,8 +40,8 @@ def run_comparison(exponential_decay_value, ransac, run_ransac_modified, prosac,
     matches_all = np.load(matches_path)
 
     #  this will hold inliers_no, outliers_no, iterations, time for each image
-    ransac_data = np.empty([0, 4])
-    ransac_images_poses = {}
+    data = np.empty([0, 4])
+    images_poses = {}
 
     ransac_dist_data = np.empty([0, 4])
     ransac_dist_images_poses = {}
@@ -34,66 +55,24 @@ def run_comparison(exponential_decay_value, ransac, run_ransac_modified, prosac,
         print("Doing image " + str(i+1) + "/" + str(len(test_images)) + ", " + image , end="\r")
 
         if(len(matches_for_image) >= 4):
-            # vanilla RANSAC
+
+            if(sort_matches_func !=None):
+                matches_for_image = sort_matches_func(matches_for_image)
+
+            if(points3D_avg_heatmap_vals.size !=0 ):
+                sub_dist = get_sub_distribution(matches_for_image, points3D_avg_heatmap_vals)
+                matches_for_image = np.hstack((matches_for_image, sub_dist))
+
             start = time.time()
-            inliers_no, outliers_no, iterations, best_model, inliers = ransac(matches_for_image)
+            inliers_no, outliers_no, iterations, best_model, inliers = func(matches_for_image)
             end  = time.time()
             elapsed_time = end - start
 
-            ransac_images_poses[image] = best_model
-            ransac_data = np.r_[ransac_data, np.array([inliers_no, outliers_no, iterations, elapsed_time]).reshape([1,4])]
-
-            # modified RANSAC with distribution
-            sub_dist = get_sub_distribution(matches_for_image, points3D_avg_heatmap_vals)
-            start = time.time()
-            inliers_no, outliers_no, iterations, best_model, inliers = run_ransac_modified(matches_for_image,sub_dist)
-            end = time.time()
-            elapsed_time = end - start
-
-            ransac_dist_images_poses[image] = best_model
-            ransac_dist_data = np.r_[ransac_dist_data, np.array([inliers_no, outliers_no, iterations, elapsed_time]).reshape([1, 4])]
-
-            # PROSAC
-            # get sorted image matches
-            # 6 is the lowes_distance_inverse, 7 is the heatmap value
-            # TODO: Normalise both ?
-            lowes_distances = matches_for_image[:, 6]
-            heatmap_vals = matches_for_image[:, 7] / matches_for_image[:, 7].sum()
-            score_list = lowes_distances * heatmap_vals # or you can use, score_list = lowes_distances
-
-            # sorted_indices
-            sorted_indices = np.argsort(score_list)
-            # in descending order
-            sorted_matches = matches_for_image[sorted_indices[::-1]]
-
-            start = time.time()
-            inliers_no, outliers_no, iterations, best_model, inliers = prosac(sorted_matches)
-            end = time.time()
-            elapsed_time_mod = end - start
-
-            prosac_images_poses[image] = best_model
-            prosac_data = np.r_[prosac_data, np.array([inliers_no, outliers_no, iterations, elapsed_time_mod]).reshape([1, 4])]
+            images_poses[image] = best_model
+            data = np.r_[data, np.array([inliers_no, outliers_no, iterations, elapsed_time]).reshape([1,4])]
 
         else:
             print(image + " has less than 4 matches..")
-
     print("\n")
-    print("Results for exponential_decay_value " + str(exponential_decay_value) + ":")
-    print("Vanillia RANSAC")
-    print("     Average Inliers: " + str(np.mean(ransac_data[:,0])))
-    print("     Average Outliers: " + str(np.mean(ransac_data[:,1])))
-    print("     Average Iterations: " + str(np.mean(ransac_data[:,2])))
-    print("     Average Time (s): " + str(np.mean(ransac_data[:,3])))
-    print("RANSAC with dist")
-    print("     Average Inliers: " + str(np.mean(ransac_dist_data[:, 0])))
-    print("     Average Outliers: " + str(np.mean(ransac_dist_data[:, 1])))
-    print("     Average Iterations: " + str(np.mean(ransac_dist_data[:, 2])))
-    print("     Average Time (s): " + str(np.mean(ransac_dist_data[:, 3])))
-    print("PROSAC")
-    print("     Average Inliers: " + str(np.mean(prosac_data[:, 0])))
-    print("     Average Outliers: " + str(np.mean(prosac_data[:, 1])))
-    print("     Average Iterations: " + str(np.mean(prosac_data[:, 2])))
-    print("     Average Time (s): " + str(np.mean(prosac_data[:, 3])))
-    print("<---->")
 
-    return ransac_images_poses, ransac_data, ransac_dist_images_poses, ransac_dist_data, prosac_images_poses, prosac_data
+    return images_poses, data
