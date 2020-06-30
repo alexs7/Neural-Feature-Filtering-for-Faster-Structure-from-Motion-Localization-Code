ac36edbc331353daf8e1fc98513830736cc86c70
alex <alexandrosrotsidis@icloud.com>
2020-06-30 12:33:16 +0100

    debugging lowes ratio test

--- main.py
+++ main.py
@@ -1,36 +1,44 @@
 # Arguments
 import cv2
+
+from database import COLMAPDatabase
 from feature_matcher import FeatureMatcherTypes, feature_matcher_factory
 from feature_matching_generator import feature_matcher_wrapper
 from parameters import Parameters
 from point3D_loader import read_points3d_default
 import numpy as np
 
-from query_image import get_images_names_bin
-from ransac_comparison import run_comparison
-from ransac_prosac import ransac, prosac
+from pose_evaluator import pose_evaluate
+from query_image import get_images_names_bin, read_images_binary, get_query_image_global_pose_new_model, get_images_names_from_sessions_numbers
+from ransac_comparison import run_comparison, plain_sort_matches, enhanced_sort_matches
+from ransac_prosac import ransac, ransac_dist, prosac
 
 features_no = "1k" # colmap_features_no can be "2k", "1k", "0.5k", "0.25k"
 exponential_decay_value = 0.5 # exponential_decay can be any of 0.1 to 0.9
 
-db_path = "/Users/alex/Projects/EngDLocalProjects/LEGO/fullpipeline/colmap_data/data/multiple_localised_models/"+features_no+"/database.db"
+db_path = Parameters.db_path
+db = COLMAPDatabase.connect(db_path)
+#  no_images_per_session[0] is base images
+no_images_per_session = Parameters.no_images_per_session
 
-base_model_images_path = "/Users/alex/Projects/EngDLocalProjects/LEGO/fullpipeline/colmap_data/all_data_and_models/coop_local/2020-03-28/coop_local/reconstruction/model/0/images.bin"
-complete_model_images_path = "/Users/alex/Projects/EngDLocalProjects/LEGO/fullpipeline/colmap_data/data/multiple_localised_models/" + features_no + "/images.bin"
+live_model_images_path = "/Users/alex/Projects/EngDLocalProjects/LEGO/fullpipeline/colmap_data/data/new_model/images.bin"
+live_model_all_images = read_images_binary(live_model_images_path)
 
-base_images_names = get_images_names_bin(base_model_images_path)
-all_images_names = get_images_names_bin(complete_model_images_path) #all = query + base images
+base_images_names = get_images_names_from_sessions_numbers([no_images_per_session[0]], db, live_model_all_images) # need to pass array here
+#all = query + base images (contains also base images - doesn't really matter here - you can see them as query images too)
+all_images_names = get_images_names_from_sessions_numbers(no_images_per_session, db, live_model_all_images) #all = query + base images
 
-train_descriptors_all_path = "/Users/alex/Projects/EngDLocalProjects/LEGO/fullpipeline/colmap_data/data/descriptors_avg/"+features_no+"/points_mean_descs_all.npy"
-train_descriptors_base_path = "/Users/alex/Projects/EngDLocalProjects/LEGO/fullpipeline/colmap_data/data/descriptors_avg/"+features_no+"/points_mean_descs_base.npy"
+# switch between these two: avg_descs_base.npy and avg_descs_all.npy (also try weighted)
+train_descriptors = "/Users/alex/Projects/EngDLocalProjects/LEGO/fullpipeline/colmap_data/data/descriptors_avg/"+features_no+"/avg_descs_all.npy"
+train_descriptors = np.load(train_descriptors).astype(np.float32)
 
-# by "complete model" I mean all the frames from future sessions localised in the base model (28/03)
-complete_model_points3D_path = "/Users/alex/Projects/EngDLocalProjects/LEGO/fullpipeline/colmap_data/data/multiple_localised_models/" + features_no + "/points3D.bin"
-points3D = read_points3d_default(complete_model_points3D_path)  # base model's 3D points (same length as complete as we do not add points when localising new points, but different image_ds for each point)
+# by "live model" I mean all the frames from future sessions localised in the base model (28/03)
+live_model_points3D_path = "/Users/alex/Projects/EngDLocalProjects/LEGO/fullpipeline/colmap_data/data/new_model/points3D.bin"
+points3D = read_points3d_default(live_model_points3D_path)  # base model's 3D points (same length as complete as we do not add points when localising new points, but different image_ids for each point)
 
 # define matcher
 matching_algo = FeatureMatcherTypes.FLANN  # or FeatureMatcherTypes.BF
-match_ratio_test = Parameters.kFeatureMatchRatioTest
+match_ratio_test = 0.2 #Parameters.kFeatureMatchRatioTest
 norm_type = cv2.NORM_L2
 cross_check = False
 matcher = feature_matcher_factory(norm_type, cross_check, match_ratio_test, matching_algo)
@@ -39,28 +47,75 @@ matcher = feature_matcher_factory(norm_type, cross_check, match_ratio_test, matc
 points3D_avg_heatmap_vals = np.loadtxt("/Users/alex/Projects/EngDLocalProjects/LEGO/fullpipeline/colmap_data/data/visibility_matrices/"+features_no+"/heatmap_matrix_avg_points_values_" + str(exponential_decay_value) + ".txt")
 points3D_avg_heatmap_vals = points3D_avg_heatmap_vals.reshape([1, points3D_avg_heatmap_vals.shape[0]])
 
-matches_path = "/Users/alex/Projects/EngDLocalProjects/LEGO/fullpipeline/colmap_data/data/feature_matching/"+features_no+"/matches_all.npy"
+# Save paths
+# Matches save locations
+matches_save_path = "/Users/alex/Projects/EngDLocalProjects/LEGO/fullpipeline/colmap_data/data/feature_matching/" + features_no + "/matches.npy"
 
-# RANSAC Comparison save locations
-vanillia_data_path_save_poses = "/Users/alex/Projects/EngDLocalProjects/LEGO/fullpipeline/colmap_data/data/RANSAC_results/"+features_no+"/vanilla_images_pose_"+str(exponential_decay_value)+".npy"
-modified_data_path_save_poses = "/Users/alex/Projects/EngDLocalProjects/LEGO/fullpipeline/colmap_data/data/RANSAC_results/"+features_no+"/modified_images_pose_"+str(exponential_decay_value)+".npy"
-vanillia_data_path_save_info = "/Users/alex/Projects/EngDLocalProjects/LEGO/fullpipeline/colmap_data/data/RANSAC_results/"+features_no+"/vanillia_data_" + str(exponential_decay_value) + ".npy"
-modified_data_path_save_info = "/Users/alex/Projects/EngDLocalProjects/LEGO/fullpipeline/colmap_data/data/RANSAC_results/"+features_no+"/modified_data_" + str(exponential_decay_value) + ".npy"
-
-# Run Code here
 # 1: Feature matching
-matches = feature_matcher_wrapper(points3D_avg_heatmap_vals, db_path, all_images_names, train_descriptors_all_path, points3D, matcher)
+# TIP: Remember we are focusing on the model (and its descs) here so the cases to test are:
+# query images , train_descriptors from live model : will match base + query images descs to live_model avg descs.
+# query images , train_descriptors from base model : will match base + query images descs images descs to base avg descs.
+matches = feature_matcher_wrapper(points3D_avg_heatmap_vals, db, all_images_names, train_descriptors, points3D, matcher, verbose = True)
 
-# print("Saving matches...")
-# save the 2D-3D matches
-np.save(matches_path, matches)
+print("Saving matches...")
+# # save the 2D-3D matches
+np.save(matches_save_path, matches)
 
-# 2: RANSAC Comparison
-vanilla_images_poses, vanilla_data, modified_images_poses, modified_data = run_comparison(0.5, ransac, prosac, matches_path, all_images_names)
+breakpoint()
 
-print("Saving RANSAC data...")
-# NOTE: folders .../RANSAC_results/"+features_no+"/... where created manually..
-np.save(vanillia_data_path_save_poses, vanilla_images_poses)
-np.save(vanillia_data_path_save_info, vanilla_data)
-np.save(modified_data_path_save_poses, modified_images_poses)
-np.save(modified_data_path_save_info, modified_data)
+# # 2: RANSAC Comparison
+# RANSAC Comparison save locations
+ransac_path_poses = "/Users/alex/Projects/EngDLocalProjects/LEGO/fullpipeline/colmap_data/data/RANSAC_results/" + features_no + "/ransac_images_pose_" + str(exponential_decay_value) + ".npy"
+ransac_path_data = "/Users/alex/Projects/EngDLocalProjects/LEGO/fullpipeline/colmap_data/data/RANSAC_results/" + features_no + "/ransac_data_" + str(exponential_decay_value) + ".npy"
+ransac_dist_path_poses = "/Users/alex/Projects/EngDLocalProjects/LEGO/fullpipeline/colmap_data/data/RANSAC_results/" + features_no + "/ransac_dist_images_pose_" + str(exponential_decay_value) + ".npy"
+ransac_dist_path_data = "/Users/alex/Projects/EngDLocalProjects/LEGO/fullpipeline/colmap_data/data/RANSAC_results/" + features_no + "/ransac_dist_data_" + str(exponential_decay_value) + ".npy"
+
+prosac_path_poses = "/Users/alex/Projects/EngDLocalProjects/LEGO/fullpipeline/colmap_data/data/RANSAC_results/" + features_no + "/prosac_images_pose_" + str(exponential_decay_value) + ".npy"
+prosac_path_data = "/Users/alex/Projects/EngDLocalProjects/LEGO/fullpipeline/colmap_data/data/RANSAC_results/" + features_no + "/prosac_data_" + str(exponential_decay_value) + ".npy"
+prosac_enh_path_poses = "/Users/alex/Projects/EngDLocalProjects/LEGO/fullpipeline/colmap_data/data/RANSAC_results/" + features_no + "/prosac_enh_images_pose_" + str(exponential_decay_value) + ".npy"
+prosac_enh_path_data = "/Users/alex/Projects/EngDLocalProjects/LEGO/fullpipeline/colmap_data/data/RANSAC_results/" + features_no + "/prosac_enh_data_" + str(exponential_decay_value) + ".npy"
+
+# RANSAC
+# poses , data = run_comparison(ransac, matches_path, query_images_names)
+# np.save(ransac_path_poses, poses)
+# np.save(ransac_path_data, data)
+# # # RANSAC + dist
+# poses , data = run_comparison(ransac_dist, matches_path, query_images_names, points3D_avg_heatmap_vals)
+# np.save(ransac_dist_path_poses, poses)
+# np.save(ransac_dist_path_data, data)
+# PROSAC lowe's
+# poses , data = run_comparison(prosac, matches_path, query_images_names, sort_matches_func = plain_sort_matches)
+# np.save(prosac_path_poses, poses)
+# np.save(prosac_path_data, data)
+# PROSAC lowes * heatmap value
+poses , data = run_comparison(prosac, matches_save_path, all_images_names, sort_matches_func = enhanced_sort_matches)
+np.save(prosac_enh_path_poses, poses)
+np.save(prosac_enh_path_data, data)
+
+ransac_data = np.load(ransac_path_data)
+ransac_dist_data = np.load(ransac_dist_path_data)
+prosac_data = np.load(prosac_path_data)
+prosac_enh_data = np.load(prosac_enh_path_data)
+
+# 3: Evaluate Poses against Ground Truth
+vanilla_images_poses = np.load(vanillia_data_path_save_poses).item() # it is a dict!
+modified_images_poses = np.load(modified_data_path_save_poses).item() # it is a dict!
+prosac_1_images_poses = np.load(prosac_1_data_path_save_poses).item() # it is a dict!
+prosac_2_images_poses = np.load(prosac_2_data_path_save_poses).item() # it is a dict!
+
+print("Evaluating Poses")
+# get gt poses
+gt_poses = {}
+complete_model_all_images = read_images_binary(complete_model_images_path)
+for image in all_images_names:
+    gt_poses[image] = get_query_image_global_pose_new_model(image, complete_model_all_images)
+
+t_error, r_error = pose_evaluate(vanilla_images_poses, gt_poses)
+print("RANSAC Errors Mean: Trans: %2.1f, Rotations: %2.1f" % (t_error.mean(), r_error.mean()))
+t_error, r_error = pose_evaluate(modified_images_poses, gt_poses)
+print("RANSAC + sampling Errors Mean: Trans: %2.1f, Rotations: %2.1f" % (t_error.mean(), r_error.mean()))
+breakpoint()
+t_error, r_error = pose_evaluate(prosac_1_images_poses, gt_poses)
+print("PROSAC ( lowe’s dist ratio * heat map vals) Mean: Trans: %2.1f, Rotations: %2.1f" % (t_error.mean(), r_error.mean()))
+t_error, r_error = pose_evaluate(prosac_2_images_poses, gt_poses)
+print("PROSAC (lowe’s dist ratio) Mean: Trans: %2.1f, Rotations: %2.1f" % (t_error.mean(), r_error.mean()))
