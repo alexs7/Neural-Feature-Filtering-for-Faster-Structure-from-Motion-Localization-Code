5f8cff2eaa97d6b28d414a3c14c3a99cc670086a
alex <alexandrosrotsidis@icloud.com>
2020-06-30 11:03:43 +0100

    remofved old code

--- export_colmap_data_to_threejs.py
+++ export_colmap_data_to_threejs.py
@@ -6,43 +6,36 @@ from point3D_loader import read_points3d_binary_id
 import sqlite3
 import sys
 import os
+from database import COLMAPDatabase
 
-IS_PYTHON3 = sys.version_info[0] >= 3
 
-class COLMAPDatabase(sqlite3.Connection):
+def savePoints3DxyzToFile(points3D_xyz):
+    np.savetxt("/Users/alex/Projects/EngDLocalProjects/LEGO/fullpipeline/colmap_data/data/threejs_data_exported/points3D.txt", points3D_xyz)
 
-    @staticmethod
-    def connect(database_path):
-        return sqlite3.connect(database_path, factory=COLMAPDatabase)
+# 28/06/2020 old code might still be useful
 
-def blob_to_array(blob, dtype, shape=(-1,)):
-    if IS_PYTHON3:
-        return np.frombuffer(blob, dtype=dtype).reshape(*shape)
-    else:
-        return np.frombuffer(blob, dtype=dtype).reshape(*shape)
-
-source_3D_points = sys.argv[1] #bin file
-dest_3D_points = sys.argv[2] #text file
+# source_3D_points = sys.argv[1] #bin file
+# dest_3D_points = sys.argv[2] #text file
 
 # clean up old files
 # os.system("rm /Users/alex/Projects/EngDLocalProjects/LEGO/fullpipeline/colmap_data/data/threejs_data_exported/*")
 
-db = COLMAPDatabase.connect("/Users/alex/Projects/EngDLocalProjects/LEGO/fullpipeline/colmap_data/data/database.db")
+# db = COLMAPDatabase.connect("/Users/alex/Projects/EngDLocalProjects/LEGO/fullpipeline/colmap_data/data/database.db")
+#
+# # 3D points
+# print("Exporting 3D Points")
+# points3D = read_points3d_default(source_3D_points)
 
-# 3D points
-print("Exporting 3D Points")
-points3D = read_points3d_default(source_3D_points)
-
-print("Total Points: " + str(len(points3D)))
+# print("Total Points: " + str(len(points3D)))
 
 # write all 3D points
-print("Writing all 3D points")
-all_points3D =  np.empty([0,4])
-for k,v in points3D.items():
-    data = np.array([v.id , v.xyz[0], v.xyz[1] , v.xyz[2]])
-    data = data.reshape(1,4)
-    all_points3D = np.r_[all_points3D, data]
-np.savetxt(dest_3D_points, all_points3D)
+# print("Writing all 3D points")
+# all_points3D =  np.empty([0,4])
+# for k,v in points3D.items():
+#     data = np.array([v.id , v.xyz[0], v.xyz[1] , v.xyz[2]])
+#     data = data.reshape(1,4)
+#     all_points3D = np.r_[all_points3D, data]
+# np.savetxt(dest_3D_points, all_points3D)
 
 # print("Writing all 3D points - RGB values")
 # all_points3D_rgb =  np.empty([0,4])
@@ -52,36 +45,36 @@ np.savetxt(dest_3D_points, all_points3D)
 #     all_points3D_rgb = np.r_[all_points3D_rgb, data]
 # np.savetxt("/Users/alex/Projects/EngDLocalProjects/LEGO/fullpipeline/colmap_data/data/threejs_data_exported/all_rgb_points3D.txt", all_points3D_rgb)
 #
-print("Getting observation count")
-observations = []
-for k,v in points3D.items():
-    observations.append(len(v.image_ids))
-observations = np.array(observations)
-observations_mean = observations.mean()
-
-print("Writing all 3D points - observation mean")
-all_points3D_obv_count =  np.empty([0,4])
-for k,v in points3D.items():
-    if(len(v.image_ids) > observations_mean ):
-        data = np.array([v.id, v.xyz[0], v.xyz[1], v.xyz[2]])
-        data = data.reshape(1, 4)
-        all_points3D_obv_count = np.r_[all_points3D_obv_count, data]
-np.savetxt("/Users/alex/Projects/EngDLocalProjects/LEGO/fullpipeline/colmap_data/data/threejs_data_exported/all_xyz_points3D_obvs_mean.txt", all_points3D_obv_count)
-
-print("Getting mean desc for each 3D point") #one file for each
-for k,v in points3D.items():
-    points3D_descs =  np.empty([0,128])
-    for i in range(len(v.image_ids)): #TODO: this might need to be unique? no! because indexing is important here
-        img_id = v.image_ids[i]
-        data = db.execute("SELECT data FROM descriptors WHERE image_id = " + "'" + str(img_id) + "'")
-        data = blob_to_array(data.fetchone()[0], np.uint8)
-        descs_rows = int(np.shape(data)[0] / 128)
-        descs = data.reshape([descs_rows, 128])
-        desc = descs[v.point2D_idxs[i]]
-        desc = desc.reshape(1, 128)
-        points3D_descs = np.r_[points3D_descs, desc]
-    # np.savetxt("/Users/alex/Projects/EngDLocalProjects/LEGO/fullpipeline/colmap_data/data/threejs_data_exported/desc_for_point3D_" + str(v.id)+".txt", points3D_descs)
-    np.savetxt("/Users/alex/Projects/EngDLocalProjects/LEGO/fullpipeline/colmap_data/data/threejs_data_exported/mean_decs/mean_for_descs_for_point3D_" + str(v.id)+".txt", points3D_descs.mean(axis=0).reshape(1,128))
+# print("Getting observation count")
+# observations = []
+# for k,v in points3D.items():
+#     observations.append(len(v.image_ids))
+# observations = np.array(observations)
+# observations_mean = observations.mean()
+#
+# print("Writing all 3D points - observation mean")
+# all_points3D_obv_count =  np.empty([0,4])
+# for k,v in points3D.items():
+#     if(len(v.image_ids) > observations_mean ):
+#         data = np.array([v.id, v.xyz[0], v.xyz[1], v.xyz[2]])
+#         data = data.reshape(1, 4)
+#         all_points3D_obv_count = np.r_[all_points3D_obv_count, data]
+# np.savetxt("/Users/alex/Projects/EngDLocalProjects/LEGO/fullpipeline/colmap_data/data/threejs_data_exported/all_xyz_points3D_obvs_mean.txt", all_points3D_obv_count)
+#
+# print("Getting mean desc for each 3D point") #one file for each
+# for k,v in points3D.items():
+#     points3D_descs =  np.empty([0,128])
+#     for i in range(len(v.image_ids)): #TODO: this might need to be unique? no! because indexing is important here
+#         img_id = v.image_ids[i]
+#         data = db.execute("SELECT data FROM descriptors WHERE image_id = " + "'" + str(img_id) + "'")
+#         data = blob_to_array(data.fetchone()[0], np.uint8)
+#         descs_rows = int(np.shape(data)[0] / 128)
+#         descs = data.reshape([descs_rows, 128])
+#         desc = descs[v.point2D_idxs[i]]
+#         desc = desc.reshape(1, 128)
+#         points3D_descs = np.r_[points3D_descs, desc]
+#     # np.savetxt("/Users/alex/Projects/EngDLocalProjects/LEGO/fullpipeline/colmap_data/data/threejs_data_exported/desc_for_point3D_" + str(v.id)+".txt", points3D_descs)
+#     np.savetxt("/Users/alex/Projects/EngDLocalProjects/LEGO/fullpipeline/colmap_data/data/threejs_data_exported/mean_decs/mean_for_descs_for_point3D_" + str(v.id)+".txt", points3D_descs.mean(axis=0).reshape(1,128))
 
 # Rest of colmap data (poses)
 # print("Exporting Rest of Data..")
