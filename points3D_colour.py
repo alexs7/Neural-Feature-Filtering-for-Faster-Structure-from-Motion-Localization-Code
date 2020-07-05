4e056c88a5569f5120c23c79020f5cf21058e7de
alex <alexandrosrotsidis@icloud.com>
2020-07-05 23:19:19 +0100

    added 3D graphs

--- points3D_colour.py
+++ points3D_colour.py
@@ -1,4 +1,5 @@
 import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
 
 from export_colmap_data_to_threejs import savePoints3DxyzToFile
 from point3D_loader import read_points3d_default, index_dict
@@ -8,42 +9,73 @@ from sklearn.cluster import KMeans
 features_no = "1k"
 exponential_decay_value = "0.5"
 
-complete_model_points3D_path = "/Users/alex/Projects/EngDLocalProjects/LEGO/fullpipeline/colmap_data/data/multiple_localised_models/" + features_no + "/points3D.bin"
-points3D = read_points3d_default(complete_model_points3D_path)
+live_model_points3D_path = "/Users/alex/Projects/EngDLocalProjects/LEGO/fullpipeline/colmap_data/data/new_model/points3D.bin"
+points3D = read_points3d_default(live_model_points3D_path)
 points3D_indexing = index_dict(points3D)
 
-rgb_avg = []
-for k,v in points3D.items():
-    rgb_avg.append(v.rgb.mean())
+sorted_matches = np.load("/Users/alex/Projects/EngDLocalProjects/LEGO/fullpipeline/colmap_data/data/sorted_matches.npy")
 
-rgb_avg = np.array(rgb_avg)
-rgb_avg = rgb_avg.reshape([rgb_avg.shape[0] , 1])
+xs = []
+ys = []
+sorting_vals = []
+for i in range(len(sorted_matches)):
+    x = sorted_matches[i,0]
+    y = sorted_matches[i,1]
+    val = sorted_matches[i, 6] * sorted_matches[i, 7]
+    xs.append(x)
+    ys.append(y)
+    sorting_vals.append(val)
 
-heatmap_matrix = np.loadtxt("/Users/alex/Projects/EngDLocalProjects/LEGO/fullpipeline/colmap_data/data/visibility_matrices/"+features_no+"/heatmap_matrix_" + str(exponential_decay_value) + ".txt")
-points3D_sum_heatmap_vals = heatmap_matrix.sum(axis=0) #axis = 0, apply summation along rows axis of array, i.e sum columns
-points3D_sum_heatmap_vals = points3D_sum_heatmap_vals.reshape([points3D_sum_heatmap_vals.shape[0] , 1])
+    # points3D_index = sorted_matches[i, 5]
+    # point3D_id = points3D_indexing[points3D_index]
+    # current_point3D = points3D[point3D_id]
 
-data  = np.concatenate([points3D_sum_heatmap_vals, rgb_avg], axis=1)
+
+xs = np.array(xs)
+xs = xs.reshape([xs.shape[0] , 1])
+
+ys = np.array(ys)
+ys = ys.reshape([ys.shape[0] , 1])
+
+sorting_vals = np.array(sorting_vals)
+sorting_vals = sorting_vals.reshape([sorting_vals.shape[0] , 1])
+
+data  = np.concatenate([xs, ys, sorting_vals], axis=1)
 
 print("Fitting Data")
-kmeans = KMeans(n_clusters=10)
+kmeans = KMeans(n_clusters=4)
 kmeans.fit(data)
 y_kmeans = kmeans.predict(data)
 centers = kmeans.cluster_centers_
 
 fig, (ax1, ax2) = plt.subplots(1, 2)
-fig.suptitle('Exponential Decay Values vs RGB mean value (per points). No K-means / K-means Clusters: ' + str(kmeans.n_clusters), fontsize=10)
+fig.suptitle('Pixel and Exponential Values in 3D: ' + str(kmeans.n_clusters), fontsize=10)
 
-ax1.scatter(data[:, 0], data[:, 1], s=0.4)
-ax1.set_xlabel('Exponential Decay Value (the higher the more static the point)', fontsize=9)
-ax1.set_ylabel('Mean RGB value of 3D points', fontsize=9)
+ax1=Axes3D(fig)
+ax2=Axes3D(fig)
 
-ax2.scatter(data[:, 0], data[:, 1], c=y_kmeans, s=0.4, cmap='jet')
-ax2.scatter(centers[:, 0], centers[:, 1], c='red', s=32, alpha=0.7)
-ax2.set_xlabel('Exponential Decay Value (the higher the more static the point)', fontsize=9)
-ax2.set_ylabel('Mean RGB value of 3D points', fontsize=9)
+# ax1.scatter(data[:, 0], data[:, 1], data[:, 2], s=0.4)
+# ax1.set_xlabel('Exponential Decay Value (the higher the more static the point)', fontsize=9)
+# ax1.set_ylabel('Mean RGB value of 3D points', fontsize=9)
+
+point_closest_to_c1 = data[np.argmin(kmeans.transform(data)[:,0]),:]
+point_closest_to_c2 = data[np.argmin(kmeans.transform(data)[:,1]),:]
+point_closest_to_c3 = data[np.argmin(kmeans.transform(data)[:,2]),:]
+point_closest_to_c4 = data[np.argmin(kmeans.transform(data)[:,3]),:]
+
+ax2.scatter(data[:, 0], data[:, 1], data[:, 2], c=y_kmeans, s=20, cmap='jet')
+ax2.scatter(point_closest_to_c1[0], point_closest_to_c1[1], point_closest_to_c1[2], c='red', s=120, alpha=0.9)
+ax2.scatter(point_closest_to_c2[0], point_closest_to_c2[1], point_closest_to_c2[2], c='red', s=120, alpha=0.9)
+ax2.scatter(point_closest_to_c3[0], point_closest_to_c3[1], point_closest_to_c3[2], c='red', s=120, alpha=0.9)
+ax2.scatter(point_closest_to_c4[0], point_closest_to_c4[1], point_closest_to_c4[2], c='red', s=120, alpha=0.9)
+ax2.scatter(centers[:, 0], centers[:, 1], centers[:, 2], c='green', s=60, alpha=0.7)
+
+ax2.set_xlabel("X Values (Width)", fontsize=9)
+ax2.set_ylabel("Y Values (Height)", fontsize=9)
+ax2.set_zlabel('Exponential Decay Value (the higher the more static the point)', fontsize=9)
 
 plt.show()
+
 
 breakpoint()
 
