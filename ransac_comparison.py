68a361062cd6b4de5719d8e8633ce3c0c6389cec
alex <alexandrosrotsidis@icloud.com>
2020-07-07 15:24:44 +0100

    added new data in ransac comparison

--- ransac_comparison.py
+++ ransac_comparison.py
@@ -16,66 +16,24 @@ def get_sub_distribution(matches_for_image, distribution):
 
 # functions for PROSAC's score list
 def enhanced_sort_matches(matches):
-    lowes_distances = matches[:, 6]
-    heatmap_vals = matches[:, 7]
-    score_list = lowes_distances * heatmap_vals
+    lowes_distances = matches[:, 6] / matches[:,6].sum()
+    scores = matches[:, 7] / matches[:,7].sum()
+    score_list = lowes_distances * scores
     # sorted_indices
     sorted_indices = np.argsort(score_list)
     # in descending order
     sorted_matches = matches[sorted_indices[::-1]]
     return sorted_matches
 
-def enhanced_sort_matches_kmeans(matches):
-    xs = []
-    ys = []
-    sorting_vals = []
-    for i in range(len(matches)):
-        x = matches[i, 0]
-        y = matches[i, 1]
-        val = matches[i, 6] * matches[i, 7] #lowes_distances * heatmap_vals
-        xs.append(x)
-        ys.append(y)
-        sorting_vals.append(val)
-    xs = np.array(xs)
-    xs = xs.reshape([xs.shape[0], 1])
-    ys = np.array(ys)
-    ys = ys.reshape([ys.shape[0], 1])
-    sorting_vals = np.array(sorting_vals)
-    sorting_vals = sorting_vals.reshape([sorting_vals.shape[0], 1])
-    data = np.concatenate([xs, ys, sorting_vals], axis=1)
-    kmeans = KMeans(n_clusters=4)
-    kmeans.fit(data)
-
-    # matches indices of centroids
-    idx0 = np.argmin(kmeans.transform(data)[:, 0])
-    idx1 = np.argmin(kmeans.transform(data)[:, 1])
-    idx2 = np.argmin(kmeans.transform(data)[:, 2])
-    idx3 = np.argmin(kmeans.transform(data)[:, 3])
-
-    top_idx = [idx0, idx1, idx2, idx3]
-    remaining_matches = np.delete(matches, top_idx, axis=0)
-    lowes_distances = remaining_matches[:, 6]
-    heatmap_vals = remaining_matches[:, 7]
-    score_list = lowes_distances * heatmap_vals
-    # sorted_indices
-    sorted_indices = np.argsort(score_list)
-    # in descending order
-    remaining_sorted_matches = remaining_matches[sorted_indices[::-1]]
-    top_matches = matches[top_idx,:]
-    sorted_matches = np.r_[top_matches, remaining_sorted_matches]
-
-    return sorted_matches
-
-def plain_sort_matches(matches):
-    lowes_distances = matches[:, 6]
-    score_list = lowes_distances
+def sort_matches(matches, idx):
+    score_list = matches[:, idx]
     # sorted_indices
     sorted_indices = np.argsort(score_list)
     # in descending order
     sorted_matches = matches[sorted_indices[::-1]]
     return sorted_matches
 
-def run_comparison(func, matches_path, test_images, points3D_avg_heatmap_vals = np.array([]) , sort_matches_func = None):
+def run_comparison(func, matches_path, test_images, dist_vals = np.array([]), sort_matches_func = None, val_idx = None):
 
     # Question: why not using matches_base ?!?!! and comparing to that ?
     # Answer: and compare what ? ransac will take less time with matches_base given the smaller number of all matches,
@@ -93,15 +51,16 @@ def run_comparison(func, matches_path, test_images, points3D_avg_heatmap_vals = 
 
         if(len(matches_for_image) >= 4):
 
-            if(sort_matches_func !=None):
-                matches_for_image = sort_matches_func(matches_for_image)
+            if(sort_matches_func !=None and val_idx != None):
+                matches_for_image = sort_matches_func(matches_for_image,val_idx)
 
-            if(points3D_avg_heatmap_vals.size !=0 ):
-                sub_dist = get_sub_distribution(matches_for_image, points3D_avg_heatmap_vals)
+            if(dist_vals.size !=0):
+                dist_vals = dist_vals / dist_vals.sum()
+                sub_dist = get_sub_distribution(matches_for_image, dist_vals)
                 matches_for_image = np.hstack((matches_for_image, sub_dist))
 
             start = time.time()
-            inliers_no, outliers_no, iterations, best_model, inliers = func(matches_for_image)
+            inliers_no, outliers_no, iterations, best_model, inliers = func(matches_for_image,image)
             end  = time.time()
             elapsed_time = end - start
 
