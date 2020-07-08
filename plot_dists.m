b82d246a5c5da5bcd8e35ff809feedccaaa94490
alex <alexandrosrotsidis@icloud.com>
2020-07-08 17:44:57 +0100

    matlab stuff

--- plot_dists.m
+++ plot_dists.m
@@ -2,10 +2,9 @@ close all;
 
 dist = importdata('/Users/alex/Projects/EngDLocalProjects/LEGO/fullpipeline/colmap_data/data/visibility_matrices/1k/heatmap_matrix_avg_points_values_0.5.txt');
 figure
-points_static_indices = dist > mean(dist);
+bar(dist);
 
-bar(dist(points_static_indices));
-
-dist = importdata('/Users/alex/Projects/EngDLocalProjects/LEGO/fullpipeline/colmap_data/data/rgb_avg.txt');
+dist1 = importdata('/Users/alex/Projects/EngDLocalProjects/LEGO/fullpipeline/colmap_data/data/visibility_matrices/1k/reliability_scores_0.5.txt');
 figure
-bar(dist(points_static_indices));
+bar(dist1);
+
