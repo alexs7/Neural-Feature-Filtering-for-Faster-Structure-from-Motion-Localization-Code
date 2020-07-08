da4fb1877299530ef157f3c278ebb2a61f994b9f
alex <alexandrosrotsidis@icloud.com>
2020-07-08 17:44:05 +0100

    refactored file operations

--- file_operations.py
+++ file_operations.py
@@ -1,18 +1,22 @@
 import glob, os
 import subprocess
+import sys
 
-base_dir = "/Users/alex/Projects/EngDLocalProjects/Lego/fullpipeline/colmap_data/all_data_and_models/coop_local/2020-06-22/evening/run_1/only_jpgs/"
+base_dir = sys.argv[1]
 
 os.chdir(base_dir)
 files = []
-nth = 3
+nth = 2
 
 for file in glob.glob("*.jpg"):
-    print(file)
     files.append(file)
 
 sub_set = files[0::nth]
 
+files_no = 0
 for file in sub_set:
-    subprocess.call(["cp", base_dir+file,
+    subprocess.call(["cp", base_dir + "/"+file,
                             base_dir+"/sub_set/"+file])
+    files_no += 1
+
+print("Files no: " + str(files_no))
