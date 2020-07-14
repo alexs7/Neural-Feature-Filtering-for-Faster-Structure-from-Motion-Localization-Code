76c85306758af4158e7eb1278ed2d30d8d8e1b80
alex <alexandrosrotsidis@icloud.com>
2020-07-14 20:42:26 +0100

    refactored ransac comparison

--- ransac_comparison.py
+++ ransac_comparison.py
@@ -1,7 +1,4 @@
 import numpy as np
-from sklearn.cluster import KMeans
-
-from query_image import load_images_from_text_file, read_images_binary, get_query_image_global_pose_new_model
 import time
 
 # This is used with the old modified RANSAC version
@@ -14,17 +11,6 @@ def get_sub_distribution(matches_for_image, distribution):
     sub_distribution = sub_distribution.reshape([sub_distribution.shape[0], 1])
     return sub_distribution
 
-# functions for PROSAC's score list
-def enhanced_sort_matches(matches):
-    lowes_distances = matches[:, 6] / matches[:,6].sum()
-    scores = matches[:, 7] / matches[:,7].sum()
-    score_list = lowes_distances * scores
-    # sorted_indices
-    sorted_indices = np.argsort(score_list)
-    # in descending order
-    sorted_matches = matches[sorted_indices[::-1]]
-    return sorted_matches
-
 def sort_matches(matches, idx):
     score_list = matches[:, idx]
     # sorted_indices
@@ -33,12 +19,7 @@ def sort_matches(matches, idx):
     sorted_matches = matches[sorted_indices[::-1]]
     return sorted_matches
 
-def run_comparison(func, matches_path, test_images, dist_vals = np.array([]), sort_matches_func = None, val_idx = None):
-
-    # Question: why not using matches_base ?!?!! and comparing to that ?
-    # Answer: and compare what ? ransac will take less time with matches_base given the smaller number of all matches,
-    # plus the get_sub_distribution might not make sense here as for base there are no future sessions yet.
-    matches_all = np.load(matches_path)
+def run_comparison(func, matches, test_images, dist_vals = np.array([]), sort_matches_func = None, val_idx = None):
 
     #  this will hold inliers_no, outliers_no, iterations, time for each image
     data = np.empty([0, 4])
@@ -46,7 +27,7 @@ def run_comparison(func, matches_path, test_images, dist_vals = np.array([]), so
 
     for i in range(len(test_images)):
         image = test_images[i]
-        matches_for_image = matches_all.item()[image]
+        matches_for_image = matches.item()[image]
         print("Doing image " + str(i+1) + "/" + str(len(test_images)) + ", " + image , end="\r")
 
         if(len(matches_for_image) >= 4):
