31b0341913d580ba56d32915f7a67b22cfcf39e2
alex <alexandrosrotsidis@icloud.com>
2020-07-14 20:33:13 +0100

    minor name change

--- pose_evaluator.py
+++ pose_evaluator.py
@@ -8,8 +8,8 @@ from query_image import read_images_binary, get_query_image_global_pose_new_mode
 def pose_evaluate(query_poses, gt_poses):
     trans_errors = []
     rotation_errors = []
-    for image_name, _ in query_poses.items():
-        q_pose = query_poses[image_name]['Rt']
+    for image_name, _ in gt_poses.items():
+        q_pose = query_poses[image_name]
         gt_pose = gt_poses[image_name]
 
         # camera center errors
