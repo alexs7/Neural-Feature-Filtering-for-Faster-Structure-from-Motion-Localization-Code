355d4b2da1c73b7fbe55b89d844e646d3741dd8d
alex <alexandrosrotsidis@icloud.com>
2020-07-18 23:46:47 +0100

    removed check for image id in get 3D mean descs file - not needed since I have 2 models

--- get_points_3D_mean_descs.py
+++ get_points_3D_mean_descs.py
@@ -10,7 +10,7 @@ from parameters import Parameters
 from point3D_loader import read_points3d_default, index_dict
 from query_image import read_images_binary, get_images_ids, get_images_names_from_sessions_numbers
 
-def get_desc_avg(image_ids, points3D, db):
+def get_desc_avg(points3D, db):
     # Note: Look at this method this way: "I want to average descs of a 3D point that belong to certain images
     # (some from base or some from all, images) and not average all the 3D points descs."
     points_mean_descs = np.empty([0, 128])
@@ -23,16 +23,14 @@ def get_desc_avg(image_ids, points3D, db):
         # If it is seen then get the desc for each id.
         for k in range(len(points_image_ids)):
             id = points3D[point_id].image_ids[k]
-            # check if the point is viewed by the current image
-            if(id in image_ids):
-                data = db.execute("SELECT data FROM descriptors WHERE image_id = " + "'" + str(id) + "'")
-                data = COLMAPDatabase.blob_to_array(data.fetchone()[0], np.uint8)
-                descs_rows = int(np.shape(data)[0] / 128)
-                descs = data.reshape([descs_rows, 128]) #descs for the whole image
-                keypoint_index = points3D[point_id].point2D_idxs[k]
-                desc = descs[keypoint_index] #keypoints and descs are ordered the same (so I use the point2D_idxs to index descs )
-                desc = desc.reshape(1, 128) #this is the desc of keypoint with index, keypoint_index, from image with id, id.
-                points3D_descs = np.r_[points3D_descs, desc]
+            data = db.execute("SELECT data FROM descriptors WHERE image_id = " + "'" + str(id) + "'")
+            data = COLMAPDatabase.blob_to_array(data.fetchone()[0], np.uint8)
+            descs_rows = int(np.shape(data)[0] / 128)
+            descs = data.reshape([descs_rows, 128]) #descs for the whole image
+            keypoint_index = points3D[point_id].point2D_idxs[k]
+            desc = descs[keypoint_index] #keypoints and descs are ordered the same (so I use the point2D_idxs to index descs )
+            desc = desc.reshape(1, 128) #this is the desc of keypoint with index, keypoint_index, from image with id, id.
+            points3D_descs = np.r_[points3D_descs, desc]
 
         # adding and calulating the mean here!
         points_mean_descs = np.r_[points_mean_descs, points3D_descs.mean(axis=0).reshape(1,128)]
@@ -57,25 +55,16 @@ base_model_points3D = read_points3d_default(Parameters.base_model_points3D_path)
 live_model_images = read_images_binary(Parameters.live_model_images_path)
 live_model_points3D = read_points3d_default(Parameters.live_model_points3D_path)
 
-#  no_images_per_session[0] is base images
-no_images_per_session = Parameters.no_images_per_session
-
-# 2 cases base and live images points3D descs
-base_images_names = get_images_names_from_sessions_numbers([no_images_per_session[0]], db_base, base_model_images) # need to pass array here
-all_images_names = get_images_names_from_sessions_numbers(no_images_per_session, db_live, live_model_images) #all = query + base images
-
-base_images_ids = get_images_ids(base_images_names, base_model_images) #there should not be any None' values here
-all_images_ids = get_images_ids(all_images_names, live_model_images) #there should not be any None' values here
-
 # You will notice that I am using live_model_points3D in both cases, fetching avg features for the base images and the live images.
 # This is because the live_model_points3D points' images_ids hold also ids of the live and base model images, since the live model is just the
 # base model with extra images localised in it. You can use the base model for the base images but you need to make sure that the base model is exactly the
 # same as the live model, before you do. TODO: Maybe change to base to get it done with ?
 
-avgs = get_desc_avg(base_images_ids, base_model_points3D, db_base)
+# 2 cases base and live images points3D descs
+avgs = get_desc_avg(base_model_points3D, db_base)
 np.save(Parameters.avg_descs_base_path, avgs)
 
-avgs = get_desc_avg(all_images_ids, live_model_points3D, db_live)
+avgs = get_desc_avg(live_model_points3D, db_live)
 np.save(Parameters.avg_descs_live_path, avgs)
 
 
