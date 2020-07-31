b7eecbeb52c0a4eca16a5ff99debdc5fdb2ad7a4
alex <alrosh7@gmail.com>
2020-07-31 09:45:46 +0100

    added results analyzer

---
+++ results_analyzer.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+result_2 = np.load("/home/alex/fullpipeline/colmap_data/CMU_data/slice2/results.npy", allow_pickle=True).item()
+result_3 = np.load("/home/alex/fullpipeline/colmap_data/CMU_data/slice3/results.npy", allow_pickle=True).item()
+result_4 = np.load("/home/alex/fullpipeline/colmap_data/CMU_data/slice4/results.npy", allow_pickle=True).item()
+result_6 = np.load("/home/alex/fullpipeline/colmap_data/CMU_data/slice6/results.npy", allow_pickle=True).item()
+result_10 = np.load("/home/alex/fullpipeline/colmap_data/CMU_data/slice10/results.npy", allow_pickle=True).item()
+result_coop = np.load("/home/alex/fullpipeline/colmap_data/Coop_data/slice1/results.npy", allow_pickle=True).item()
+
+results = [result_2, result_3, result_4, result_6, result_10, result_coop]
+
+for result in results:
+    
