ef4d626d14af74ca4de4f5a1c44416fc08034072
alex <alrosh7@gmail.com>
2020-08-06 11:22:53 +0100

    added params files

---
+++ RANSACParameters.py
@@ -0,0 +1,12 @@
+class RANSACParameters(object):
+    # PROSAC sorting values for matches indices
+    use_ransac_dist = -1
+    lowes_distance_inverse_ratio_index = 0
+    heatmap_val_index = 2
+    reliability_score_ratio_index = 3
+    custom_score_index = 4
+    higher_neighbour_score_index = 5
+    reliability_score_index = 6
+    heatmap_val_ratio_index = 7
+    higher_neighbour_val_index = 8
+    custom_score_index_2 = 9
---
+++ parameters.py
@@ -0,0 +1,58 @@
+class Parameters(object):
+
+    base_path = "/home/alex/fullpipeline/colmap_data/CMU_data/slice2/" #trailing "/"
+
+    def __init__(self, base_path):
+        self.avg_descs_base_path = base_path+"avg_descs_base.npy"
+        self.avg_descs_live_path = base_path+"avg_descs_live.npy"
+
+        self.points3D_scores_1_path = base_path+"heatmap_matrix_avg_points_values.npy"
+        self.points3D_scores_2_path = base_path+"reliability_scores.npy"
+        self.binary_visibility_values_path = base_path + "binary_visibility_values.npy"
+
+        self.matches_base_save_path = base_path+"matches_base.npy"
+        self.matches_live_save_path = base_path+"matches_live.npy"
+
+        # 29/06/2020 - My addition
+        self.live_model_images_path = base_path+"live/model/images.bin"
+        self.base_model_images_path = base_path+"base/model/0/images.bin"
+        self.gt_model_images_path = base_path+"gt/model/images.bin"
+
+        self.live_model_points3D_path = base_path+"live/model/points3D.bin"
+        self.base_model_points3D_path = base_path+"base/model/0/points3D.bin"
+        self.gt_model_points3D_path = base_path+"gt/model/points3D.bin"
+
+        self.live_db_path = base_path+"live/database.db"
+        self.base_db_path = base_path+"base/database.db"
+        self.gt_db_path = base_path+"gt/database.db"
+
+        # not the session ones!
+        self.query_images_path = base_path+"gt/query_name.txt"
+
+        self.gt_model_cameras_path = base_path+"gt/model/cameras.bin"
+
+        self.save_results_path = base_path+"results.npy"
+
+        # Parameters.no_images_per_session: Number of images per session. This is hardcoded for now, but since images are sorted by name, i.e by time in the database,
+        # then you can use these numbers to get images from each session. The numbers need to be sorted by session though. First is number of base model images.
+        self.no_images_per_session_path = base_path+"live/session_lengths.txt"
+
+        self.ratio_test_val = 0.9
+
+        self.prosac_value_titles = { self.lowes_distance_inverse_ratio_index : "Inverse Lowe's Ratio",
+                                     self.heatmap_val_index : "Heatmap Value",
+                                     self.reliability_score_index : "Reliability Score",
+                                     self.reliability_score_ratio_index : "Reliability Score Ratio",
+                                     self.custom_score_index : "Lowes by reliability score ratio",
+                                     self.custom_score_index_2: "Lowes by heatmap value ratio",
+                                     self.higher_neighbour_score_index : "Reliability Higher Neighbour Score",
+                                     self.heatmap_val_ratio_index : "Heatmap Value Ratio",
+                                     self.higher_neighbour_val_index : "Reliability Higher Neighbour Heatmap Value" }
+
+        self.ransac_prosac_iterations = 4000 # I used 1000 for dev
+        self.ransac_prosac_error_threshold = 5.0 #8.0 default, 5 from the Benchamrking 6DoF long term localization
+
+        # This is the scale you will have to multiply your COLMAP model's acquired camera centers distance with.
+        # Pass this in pose evaluator, and it will be multiplied with the distance of the gt camera center and your estimated camera center
+        # from your COLMAP model. The reference model from CMU slice_* folders is in meters, yours is not.
+        self.CMU_scale_path = base_path+"scale.txt"
