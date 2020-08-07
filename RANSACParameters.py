0fc0ab101d84178515d4becabe48e3bd951ccf19
alex <alrosh7@gmail.com>
2020-08-07 20:07:47 +0100

    added more functions

--- RANSACParameters.py
+++ RANSACParameters.py
@@ -2,24 +2,30 @@ class RANSACParameters(object):
     # PROSAC sorting values for matches indices
     use_ransac_dist = -1
     lowes_distance_inverse_ratio_index = 0
+    higher_visibility_score_index = 1
     heatmap_val_index = 2
     reliability_score_ratio_index = 3
-    custom_score_index = 4
+    lowes_ratio_reliability_score_val_ratio_index = 4
     higher_neighbour_score_index = 5
     reliability_score_index = 6
     heatmap_val_ratio_index = 7
     higher_neighbour_val_index = 8
-    custom_score_index_2 = 9
+    lowes_ratio_heatmap_val_ratio_index = 9
+    lowes_ratio_by_higher_reliability_score_index = 10
+    lowes_ratio_by_higher_heatmap_val_index = 11
 
-    prosac_value_titles = {lowes_distance_inverse_ratio_index: "Inverse Lowe's Ratio",
-                           heatmap_val_index: "Heatmap Value",
-                           reliability_score_index: "Reliability Score",
-                           reliability_score_ratio_index: "Reliability Score Ratio",
-                           custom_score_index: "Lowes by reliability score ratio",
-                           custom_score_index_2: "Lowes by heatmap value ratio",
-                           higher_neighbour_score_index: "Reliability Higher Neighbour Score",
-                           heatmap_val_ratio_index: "Heatmap Value Ratio",
-                           higher_neighbour_val_index: "Reliability Higher Neighbour Heatmap Value"}
+    prosac_value_titles = {lowes_distance_inverse_ratio_index: "inverse_lowes_ratio",
+                           heatmap_val_index: "heatmap_value",
+                           reliability_score_index: "reliability_score",
+                           reliability_score_ratio_index: "reliability_score_ratio",
+                           lowes_ratio_reliability_score_val_ratio_index: "lowes_by_reliability_score_ratio",
+                           lowes_ratio_heatmap_val_ratio_index: "lowes_by_heatmap_value_ratio",
+                           higher_neighbour_score_index: "reliability_higher_neighbour_score",
+                           heatmap_val_ratio_index: "heatmap_value_ratio",
+                           higher_neighbour_val_index: "reliability_higher_neighbour_heatmap_value",
+                           higher_visibility_score_index: "higher_neighbour_visibility_score",
+                           lowes_ratio_by_higher_reliability_score_index: "lowes_by_higher_neighbour_reliability_score",
+                           lowes_ratio_by_higher_heatmap_val_index: "lowes_by_higher_neighbour_heatmap_value"}
 
-    ransac_prosac_iterations = 1000  # I used 1000 for dev
+    ransac_prosac_iterations = 3000  # I used 1000 for dev
     ransac_prosac_error_threshold = 5.0  # 8.0 default, 5 from the Benchamrking 6DoF long term localization
