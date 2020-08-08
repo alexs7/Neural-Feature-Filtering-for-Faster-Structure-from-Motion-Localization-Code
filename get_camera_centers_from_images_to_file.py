47c10706e72aad682a0e0b62b3e203124726bfc2
alex <alexandrosrotsidis@icloud.com>
2020-08-08 17:56:40 +0100

    getting camera centers for reference db images txt file changes

--- get_camera_centers_from_images_to_file.py
+++ get_camera_centers_from_images_to_file.py
@@ -11,11 +11,12 @@ images = read_images_binary(images_path)
 K = get_intrinsics_from_camera_bin(cameras_path, 1)
 centers = get_images_camera_centers(images)
 quats = get_images_camera_quats(images)
-principal_axis_vectors = get_images_camera_principal_axis_vectors(images,K)
+principal_axis_vectors = get_images_camera_principal_axis_vectors(images, Ks)
 
 f = open(path_to_text_file, 'w')
 for name in names:
     data = str(centers[name][0]) + " " + str(centers[name][1]) + " " + str(centers[name][2]) + " " + str(quats[name][0]) + " " + str(quats[name][1]) + " " \
-           + str(quats[name][2]) + " " + str(quats[name][3]) + " " + str(principal_axis_vectors[name][0]) + " " + str(principal_axis_vectors[name][1]) + " " + str(principal_axis_vectors[name][2]) + "\n"
+           + str(quats[name][2]) + " " + str(quats[name][3]) + " " + str(principal_axis_vectors[name][0]) + " " + str(principal_axis_vectors[name][1]) \
+           + " " + str(principal_axis_vectors[name][2]) + " " + name + "\n"
     f.write(data)
 f.close()
