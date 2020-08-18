afd4360d041a90b043d32491665b32c067029e58
alex <alrosh7@gmail.com>
2020-08-18 18:12:57 +0100

    minor changes in results analyzer

--- results_analyzer.py
+++ results_analyzer.py
@@ -3,15 +3,15 @@ import numpy as np
 # add slice 2 ?
 result_3 = np.load("/home/alex/fullpipeline/colmap_data/CMU_data/slice3/results.npy", allow_pickle=True).item()
 result_4 = np.load("/home/alex/fullpipeline/colmap_data/CMU_data/slice4/results.npy", allow_pickle=True).item()
-# result_6 = np.load("/home/alex/fullpipeline/colmap_data/CMU_data/slice6/results.npy", allow_pickle=True).item()
+result_6 = np.load("/home/alex/fullpipeline/colmap_data/CMU_data/slice6/results.npy", allow_pickle=True).item()
 result_10 = np.load("/home/alex/fullpipeline/colmap_data/CMU_data/slice10/results.npy", allow_pickle=True).item()
 result_11 = np.load("/home/alex/fullpipeline/colmap_data/CMU_data/slice11/results.npy", allow_pickle=True).item()
 
 # CMU
-results_cmu = [result_3, result_4, result_10, result_11]
+results_cmu = [result_3, result_4, result_6, result_10, result_11]
 
 # Coop
-# result_coop = np.load("/home/alex/fullpipeline/colmap_data/Coop_data/slice1/results.npy", allow_pickle=True).item()
+result_coop = np.load("/home/alex/fullpipeline/colmap_data/Coop_data/slice1/results.npy", allow_pickle=True).item()
 
 # print individual (This might not be needed for the paper)
 # for results in results_cmu:
@@ -25,8 +25,17 @@ results_cmu = [result_3, result_4, result_10, result_11]
 
 print("-------------- RANSACs/PROSAC Stats")
 
-ransac_types = ['ransac_base', 'prosac_base', 'ransac_live', 'ransac_dist_live', 'inverse_lowes_ratio', 'reliability_higher_neighbour_heatmap_value', 'reliability_higher_neighbour_score',
-                'higher_neighbour_visibility_score', 'lowes_by_reliability_score_ratio', 'lowes_by_heatmap_value_ratio', 'lowes_by_higher_neighbour_reliability_score', 'lowes_by_higher_neighbour_heatmap_value']
+# all
+# ransac_types = ['ransac_base', 'prosac_base', 'ransac_live', 'ransac_dist_heatmap_val', 'ransac_dist_reliability_score',
+#                 'ransac_dist_visibility_score', 'inverse_lowes_ratio', 'reliability_higher_neighbour_heatmap_value', 'reliability_higher_neighbour_score',
+#                 'higher_neighbour_visibility_score', 'lowes_by_reliability_score_ratio', 'lowes_by_heatmap_value_ratio',
+#                 'lowes_by_higher_neighbour_reliability_score', 'lowes_by_higher_neighbour_heatmap_value']
+# For paper
+ransac_types = ['ransac_base', 'prosac_base', 'ransac_live', 'ransac_dist_heatmap_val',
+                'ransac_dist_visibility_score', 'inverse_lowes_ratio', 'reliability_higher_neighbour_heatmap_value', 'reliability_higher_neighbour_score',
+                'higher_neighbour_visibility_score', 'lowes_by_reliability_score_ratio']
+
+print("RANSAC Types number: " + str(len(ransac_types)))
 
 print("ransac_type: Inliers | Outliers | Iters | Time | TransError | RotError (Means of all cmu slices - Means)")
 for ransac_type in ransac_types:
@@ -50,26 +59,77 @@ for ransac_type in ransac_types:
     time_mean = np.array(time).mean()
     transerror_mean = np.array(transerror).mean()
     roterror_mean = np.array(roterror).mean()
-    print(ransac_type + ":  %2.0f & %2.0f & %2.0f & %2.2f & %2.2f & %2.2f" % (inliers_mean, outliers_mean, iters_mean, time_mean, transerror_mean, roterror_mean))
+    print("%2.0f & %2.0f & %2.0f & %2.2f & %2.2f & %2.2f" % (inliers_mean, outliers_mean, iters_mean, time_mean, transerror_mean, roterror_mean) + " - " + ransac_type)
+
+print()
+print("ransac_type: Inliers | Outliers | Iters | Time | TransError | RotError (Coop - Means)")
+for ransac_type in ransac_types:
+    inliers = []
+    outliers = []
+    iters = []
+    time = []
+    transerror = []
+    roterror = []
+
+    # the results here are already mean
+    inliers_mean = result_coop[ransac_type][0]
+    outliers_mean = result_coop[ransac_type][1]
+    iters_mean = result_coop[ransac_type][2]
+    time_mean = result_coop[ransac_type][3]
+    transerror_mean = result_coop[ransac_type][4]
+    roterror_mean = result_coop[ransac_type][5]
+
+    print("%2.0f & %2.0f & %2.0f & %2.2f & %2.2f & %2.2f" % (inliers_mean, outliers_mean, iters_mean, time_mean, transerror_mean, roterror_mean) + " - " + ransac_type)
 
 print()
 print("-------------- Matches Stats")
 
 # matches numbers
-slices = ["slice3", "slice4", "slice10", "slice11"]
+slices = ["slice3", "slice4", "slice6", "slice10", "slice11"]
 print(slices)
 print("Base Matches | Live Matches - (Means)")
+print("CMU")
+all_base_matches = []
+all_live_matches = []
 for slice in slices:
     total_base_matches = []
     base_matches = np.load("/home/alex/fullpipeline/colmap_data/CMU_data/"+slice+"/matches_base.npy", allow_pickle=True).item()
     for k,v in base_matches.items():
         total_base_matches.append(v.shape[0])
     base_matches_mean = np.array(total_base_matches).sum() / len(total_base_matches)
+    all_base_matches.append(base_matches_mean)
 
     total_live_matches = []
     live_matches = np.load("/home/alex/fullpipeline/colmap_data/CMU_data/"+slice+"/matches_live.npy", allow_pickle=True).item()
     for k,v in live_matches.items():
         total_live_matches.append(v.shape[0])
     live_matches_mean = np.array(total_live_matches).sum() / len(total_live_matches)
+    all_live_matches.append(live_matches_mean)
 
-    print(str(base_matches_mean) + " & " + str(live_matches_mean))
+    print("%2.0f & %2.0f" % (base_matches_mean, live_matches_mean))
+
+print("All CMU matches")
+all_base_matches_mean = np.array(all_base_matches).mean()
+all_live_matches_mean = np.array(all_live_matches).mean()
+print("%2.0f & %2.0f" % (all_base_matches_mean , all_live_matches_mean))
+
+print("Coop")
+base_matches = np.load("/home/alex/fullpipeline/colmap_data/Coop_data/slice1/matches_base.npy", allow_pickle=True).item()
+live_matches = np.load("/home/alex/fullpipeline/colmap_data/Coop_data/slice1/matches_live.npy", allow_pickle=True).item()
+total_base_matches = []
+for k, v in base_matches.items():
+    total_base_matches.append(v.shape[0])
+base_matches_mean = np.array(total_base_matches).sum() / len(total_base_matches)
+
+total_live_matches = []
+for k, v in live_matches.items():
+    total_live_matches.append(v.shape[0])
+live_matches_mean = np.array(total_live_matches).sum() / len(total_live_matches)
+
+print("%2.0f & %2.0f" % (base_matches_mean , live_matches_mean))
+
+
+
+
+
+
