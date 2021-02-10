aa1b74701aeec56ed0ba7cbca8b9609b6b6f677b
alex <alrosh7@gmail.com>
2021-02-10 07:26:48 +0000

    new way to create ML data

--- create_ML_training_data.py
+++ create_ML_training_data.py
@@ -2,6 +2,7 @@ import pdb
 import sys
 from collections import defaultdict
 import numpy as np
+import pandas as pd
 from database import COLMAPDatabase
 from parameters import Parameters
 from point3D_loader import read_points3d_default, index_dict_reverse
@@ -14,43 +15,54 @@ def get_image_decs(db, image_id):
     descs = data.reshape([descs_rows, 128])  # descs for the whole image
     return descs
 
-def create_training_data(points3D, points3D_id_index, points3D_scores, images_bin, db):
-    training_data = np.empty([0, 129])
-    save_idx = 0
-    cached_image_descs = defaultdict(lambda: np.array([]))
+def get_point3D_score(points3D_scores, current_point3D_id, points3D_id_index):
+    point_index = points3D_id_index[current_point3D_id]
+    point_score = points3D_scores[point_index]
+    return point_score
 
-    for k,v in points3D.items():
-        point_id = v.id
-        point_index = points3D_id_index[point_id]
-        point_score = points3D_scores[point_index]
-        points_image_ids = points3D[point_id].image_ids #COLMAP adds the image twice some times.
-        print("Doing point " + str(point_index + 1) + "/" + str(len(points3D.items())) + ". This point is viewed from " + str(len(points_image_ids)) + " images. Training Data saves: " + str(save_idx), end="\r")
-        # Loop through the points' image ids and check if it is seen by any image_ids
-        # If it is seen then get the desc for each id.
-        for img_id_idx in range(len(points_image_ids)):
-            training_data_row = np.zeros([1, 129])
-            id = points_image_ids[img_id_idx] #id here is image_id
-            descs = np.array([])
-            if(cached_image_descs[id].size == 0):
-                cached_image_descs[id] = get_image_decs(db, id)
-                descs = cached_image_descs[id]
+def create_training_data(base_path, points3D, points3D_id_index, points3D_scores, images, db):
+    dims = 134
+    training_data = pd.DataFrame()
+    img_index = -1
+    save_idx = -1
+    for img_id , img_data in images.items():
+        print("Doing image " + str(img_index + 1) + "/" + str(len(images.items())), end="\r")
+        descs = get_image_decs(db, img_id)
+        assert(img_data.xys.shape[0] == img_data.point3D_ids.shape[0] == descs.shape[0])
+        for i in range(img_data.point3D_ids.shape[0]): # can loop through descs or img_data.xys - same thing
+            current_point3D_id = img_data.point3D_ids[i]
+
+            if(current_point3D_id == -1):
+                score = 0
             else:
-                descs = cached_image_descs[id]
-            assert(descs.size !=0 )
-            keypoint_index = points3D[point_id].point2D_idxs[img_id_idx]
-            desc = descs[keypoint_index] #keypoints and descs are ordered the same (so I use the point2D_idxs to index descs, it makes sense as the only way to get the SIFT desc is from the db)
-            desc = desc.reshape(1, 128) #this is the desc of keypoint with index, keypoint_index, from image with id, id.
-            # desc = desc / desc.sum()
+                score = get_point3D_score(points3D_scores, current_point3D_id, points3D_id_index)
+
+            if (current_point3D_id == -1):
+                xyz = np.array([0, 0, 0]) # safe to use as no image point will ever match to 0,0,0
+            else:
+                xyz = points3D[current_point3D_id].xyz
+
+            desc = descs[i]
+            xy = img_data.xys[i]
+            img_name = img_data.name
+            training_data_row = np.zeros([1, dims])
             training_data_row[0, 0:128] = desc
-            training_data_row[0, 128] = point_score
-            training_data = np.r_[training_data, training_data_row]
+            training_data_row[0, 128] = score
+            training_data_row[0, 129:132] = xyz
+            training_data_row[0, 132:134] = xy
 
-            if(training_data.shape[0] >= 5000):
+            df_row = pd.DataFrame({img_name: training_data_row[0]}).transpose()
+            training_data = pd.concat([training_data, df_row], axis=0)
+
+            if (training_data.shape[0] >= 2500):
                 save_idx += 1
-                np.save("/home/alex/fullpipeline/colmap_data/Coop_data/slice1/live_model_training_data/training_data_"+str(save_idx)+".npy", training_data)
-                training_data = np.empty([0, 129])
+                training_data.to_csv(base_path + "live_model_training_data/training_data_" + str(save_idx) + ".csv")
+                training_data = pd.DataFrame()
 
-    return training_data
+        img_index +=1
+    # save remaining rows - at this point whatever is left just save it
+    save_idx += 1
+    training_data.to_csv(base_path+"live_model_training_data/training_data_"+str(save_idx)+".csv")
 
 base_path = sys.argv[1] # example: "/home/alex/fullpipeline/colmap_data/CMU_data/slice2/" #trailing "/"
 parameters = Parameters(base_path)
@@ -64,5 +76,6 @@ points3D_per_image_decay_scores = np.load(parameters.per_image_decay_matrix_path
 points3D_per_image_decay_scores = points3D_per_image_decay_scores.sum(axis=0)
 points3D_id_index = index_dict_reverse(live_model_points3D)
 
-training_data = create_training_data(live_model_points3D, points3D_id_index, points3D_per_image_decay_scores, live_model_images, db_live)
-np.save("/home/alex/fullpipeline/colmap_data/Coop_data/slice1/live_model_training_data/training_data_last.npy", training_data)
+# i.e /home/alex/fullpipeline/colmap_data/alfa_mega/slice1/, the base model with all the sessions localised in it
+base_path = sys.argv[1]
+training_data = create_training_data(base_path, live_model_points3D, points3D_id_index, points3D_per_image_decay_scores, live_model_images, db_live)
