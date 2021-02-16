70f7e4f57f1e248e95d1461814a7b1fcb35a679c
alex <alrosh7@gmail.com>
2021-02-16 12:36:05 +0000

    added new regressio ncode

--- regression.py
+++ regression.py
@@ -1,18 +1,72 @@
-import pdb
-from sklearn import svm
+import os
+
 import numpy as np
+from sklearn.model_selection import KFold, cross_val_score, train_test_split
+from tensorflow import estimator
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Dense
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import MinMaxScaler
+import tensorflow.keras.backend as K
+import sys
+import glob
+import pandas as pd
 
-idxs = [156, 144, 192, 204, 168, 1, 4, 5, 90, 3, 111]
-all_data = np.empty([0,129])
+def soft_acc(y_true, y_pred):
+    return K.mean(K.equal(K.round(y_true), K.round(y_pred)))
 
-for idx in idxs:
-    data = np.load("/home/alex/fullpipeline/colmap_data/Coop_data/slice1/live_model_training_data/training_data_"+str(idx)+".npy")
-    all_data = np.r_[all_data, data]
+base_path = sys.argv[1]
+no_csv = len(glob.glob1(base_path,"*.csv"))
 
-X = all_data[:,0:128]
-y = all_data[:,128]
+target_index = 129 # score index
+csv_file_index = -1
 
-regr = svm.SVR(kernel='linear')
-regr.fit(X, y)
+print("Loading data files..")
+all_csvs = glob.glob(os.path.join(base_path, "*.csv"))
+df_from_each_csv = (pd.read_csv(f) for f in all_csvs)
+all_data = pd.concat(df_from_each_csv)
 
-pdb.set_trace()
+breakpoint()
+print("Splitting data into test/train..")
+X_train, X_test, y_train, y_test = train_test_split(all_data, all_data.iloc[:, target_index], test_size=0.2, shuffle=False)
+
+X = X_train.iloc[:, 1:129]
+y = y_train.values
+
+print("Data Preprocessing..")
+sc = StandardScaler()
+X = sc.fit_transform(X)
+
+min_max_scaler = MinMaxScaler()
+y = min_max_scaler.fit_transform(y.reshape(-1, 1))
+
+# create model
+print("Creating model")
+model = Sequential()
+model.add(Dense(128, input_dim=128, kernel_initializer='normal', activation='relu')) #I know all input values will be positive at this point (SIFT)
+model.add(Dense(32, input_dim=128, kernel_initializer='normal', activation='relu'))
+model.add(Dense(1, kernel_initializer='normal', activation='sigmoid'))
+
+# Compile model
+model.compile(optimizer='rmsprop', loss='mse', metrics=[soft_acc])
+
+# train
+print("Training..")
+model.fit(X, y, epochs=15, batch_size=1600)
+
+print("Evaluate Model..")
+X = X_test.iloc[:, 1:129]
+y = y_test.values
+
+X = sc.fit_transform(X)
+y = min_max_scaler.fit_transform(y.reshape(-1, 1))
+
+model.evaluate(X,y)
+
+breakpoint()
+
+# evaluation
+# estimator = KerasRegressor(build_fn=model, epochs=100, batch_size=20, verbose=0)
+# kfold = KFold(n_splits=10)
+# results = cross_val_score(estimator, X, Y, cv=kfold)
+# print("Results: %.2f (%.2f) MSE" % (results.mean(), results.std()))
