b0a8025713854b1021c82183bbd89c029e8582f7
alex <alrosh7@gmail.com>
2021-02-22 19:24:58 +0000

    added sqlite supprt

--- create_ML_training_data.py
+++ create_ML_training_data.py
@@ -2,7 +2,6 @@ import pdb
 import sys
 from collections import defaultdict
 import numpy as np
-import pandas as pd
 from database import COLMAPDatabase
 from parameters import Parameters
 from point3D_loader import read_points3d_default, index_dict_reverse
@@ -20,50 +19,34 @@ def get_point3D_score(points3D_scores, current_point3D_id, points3D_id_index):
     point_score = points3D_scores[point_index]
     return point_score
 
-def create_training_data(base_path, points3D, points3D_id_index, points3D_scores, images, db):
-    dims = 134
-    training_data = pd.DataFrame()
+def create_training_data(base_path, ml_db, points3D, points3D_id_index, points3D_scores, images, db):
     img_index = -1
-    save_idx = -1
     for img_id , img_data in images.items():
         print("Doing image " + str(img_index + 1) + "/" + str(len(images.items())), end="\r")
         descs = get_image_decs(db, img_id)
-        assert(img_data.xys.shape[0] == img_data.point3D_ids.shape[0] == descs.shape[0])
+        assert(img_data.xys.shape[0] == img_data.point3D_ids.shape[0] == descs.shape[0]) # just for my sanity
         for i in range(img_data.point3D_ids.shape[0]): # can loop through descs or img_data.xys - same thing
             current_point3D_id = img_data.point3D_ids[i]
 
             if(current_point3D_id == -1):
-                score = 0
+                score = -99.0
             else:
                 score = get_point3D_score(points3D_scores, current_point3D_id, points3D_id_index)
 
             if (current_point3D_id == -1):
                 xyz = np.array([0, 0, 0]) # safe to use as no image point will ever match to 0,0,0
             else:
-                xyz = points3D[current_point3D_id].xyz
+                xyz = points3D[current_point3D_id].xyz #np.float64
 
-            desc = descs[i]
-            xy = img_data.xys[i]
+            desc = descs[i] # np.uint8
+            xy = img_data.xys[i] #np.float64, same as xyz
             img_name = img_data.name
-            training_data_row = np.zeros([1, dims])
-            training_data_row[0, 0:128] = desc
-            training_data_row[0, 128] = score
-            training_data_row[0, 129:132] = xyz
-            training_data_row[0, 132:134] = xy
 
-            df_row = pd.DataFrame({img_name: training_data_row[0]}).transpose()
-            training_data = pd.concat([training_data, df_row], axis=0)
-
-            if (training_data.shape[0] >= 2500):
-                save_idx += 1
-                training_data.to_csv(base_path + "live_model_training_data/training_data_" + str(save_idx) + ".csv")
-                training_data = pd.DataFrame()
-
+            ml_db.execute("INSERT INTO data VALUES (?, ?, ?, ?, ?, ?)",
+                            (img_id,) + (img_name,) + (COLMAPDatabase.array_to_blob(desc),) + (score,) +
+                              (COLMAPDatabase.array_to_blob(xyz),) + (COLMAPDatabase.array_to_blob(xy),))
         img_index +=1
-    # save remaining rows - at this point whatever is left just save it
-    save_idx += 1
-    training_data.to_csv(base_path+"live_model_training_data/training_data_"+str(save_idx)+".csv")
-
+    ml_db.commit()
 base_path = sys.argv[1] # example: "/home/alex/fullpipeline/colmap_data/CMU_data/slice2/" #trailing "/"
 parameters = Parameters(base_path)
 
@@ -76,6 +59,7 @@ points3D_per_image_decay_scores = np.load(parameters.per_image_decay_matrix_path
 points3D_per_image_decay_scores = points3D_per_image_decay_scores.sum(axis=0)
 points3D_id_index = index_dict_reverse(live_model_points3D)
 
-# i.e /home/alex/fullpipeline/colmap_data/alfa_mega/slice1/, the base model with all the sessions localised in it
-base_path = sys.argv[1]
-training_data = create_training_data(base_path, live_model_points3D, points3D_id_index, points3D_per_image_decay_scores, live_model_images, db_live)
+# i.e /home/alex/fullpipeline/colmap_data/alfa_mega/slice1/ML_data/database.db
+ml_db_path = sys.argv[2]
+ml_data_db = COLMAPDatabase.create_connection(ml_db_path)
+training_data = create_training_data(base_path, ml_data_db, live_model_points3D, points3D_id_index, points3D_per_image_decay_scores, live_model_images, db_live)
--- database.py
+++ database.py
@@ -1,4 +1,5 @@
 import sqlite3
+from sqlite3 import Error
 import sys
 import numpy as np
 
@@ -16,3 +17,32 @@ class COLMAPDatabase(sqlite3.Connection):
             return np.frombuffer(blob, dtype=dtype).reshape(*shape)
         else:
             return np.frombuffer(blob, dtype=dtype).reshape(*shape)
+
+    @staticmethod
+    def array_to_blob(array):
+        if IS_PYTHON3:
+            return array.tostring()
+        else:
+            return np.getbuffer(array)
+
+    @staticmethod
+    def create_connection(db_file):
+        sql_drop_table_if_exists = "DROP TABLE IF EXISTS data;"
+        sql_create_data_table = """CREATE TABLE IF NOT EXISTS data (
+                                                image_id INTEGER NOT NULL,
+                                                name TEXT NOT NULL,
+                                                sift BLOB NOT NULL,
+                                                score FLOAT NOT NULL,
+                                                xyz BLOB NOT NULL,
+                                                xy BLOB NOT NULL
+                                            );"""
+        conn = None
+        try:
+            conn = sqlite3.connect(db_file)
+            conn.execute(sql_drop_table_if_exists)
+            conn.commit()
+            conn.execute(sql_create_data_table)
+            conn.commit()
+            return conn
+        except Error as e:
+            print(e)
