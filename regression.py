dd02918653f906fe456a358eb76ec184aef4aa09
alex <ar2056@bath.ac.uk>
2021-03-01 19:20:00 +0000

    fixing rgression.. problems with memory..

--- regression.py
+++ regression.py
@@ -10,6 +10,8 @@ import sys
 import glob
 import pandas as pd
 from database import COLMAPDatabase
+from sklearn.model_selection import KFold
+from sklearn.model_selection import train_test_split
 
 # this might cause problems when loading the model
 # def soft_acc(y_true, y_pred):
@@ -44,7 +46,9 @@ all_scores = (score[0] for score in scores)
 all_scores = np.array(list(all_scores))
 
 print("Splitting data into test/train..")
-X_train, y_train, X_test, y_test = split_data(all_sifts, all_scores, 0.3, randomize = True)
+
+# X_train, y_train, X_test, y_test = split_data(all_sifts, all_scores, 0.3, randomize = True)
+X_train, X_test, y_train, y_test = train_test_split(all_sifts, all_scores, test_size=0.3, shuffle=True, random_state=42)
 
 # standard scaling - mean normalization
 X_train = ( X_train - X_train.mean() ) / X_train.std()
@@ -53,43 +57,56 @@ X_test = ( X_test - X_test.mean() ) / X_test.std()
 y_train = ( y_train - y_train.min() ) / ( y_train.max() - y_train.min() )
 y_test = ( y_test - y_test.min() ) / ( y_test.max() - y_test.min() )
 
-# create model
-print("Creating model")
-model = Sequential()
-# in keras the first layer is a hidden layer too, so input dims is OK here
-model.add(Dense(128, input_dim=128, kernel_initializer='normal', activation='relu')) #I know all input values will be positive at this point (SIFT)
-model.add(Dense(64, kernel_initializer='normal', activation='relu'))
-model.add(Dense(32, kernel_initializer='normal', activation='relu'))
-model.add(Dense(16, kernel_initializer='normal', activation='relu'))
-model.add(Dense(8, kernel_initializer='normal', activation='relu'))
-model.add(Dense(1, kernel_initializer='normal', activation='sigmoid')) #TODO: relu might be more appropriate here (since score can never be negative)
-
-# Compile model
-model.compile(optimizer='adam', loss='mae')
-
-# train
-print("Training.. on " + str(X_train.shape[0]) + " samples")
-history = model.fit(X_train, y_train, epochs=500, batch_size=3200, validation_split=0.3, shuffle=False, verbose='1')
-
-print(history.history.keys())
-# "Loss"
-plt.plot(history.history['loss'])
-plt.plot(history.history['val_loss'])
-plt.title('model loss')
-plt.ylabel('loss')
-plt.xlabel('epoch')
-plt.legend(['Training Loss (MSE)', 'Validation Loss (MSE)'], loc='upper left')
-plt.savefig(db_path.rsplit('/', 1)[0]+"/loss.png")
-plt.show()
-
-print("Evaluate Model..")
-model.evaluate(X_test, y_test, verbose=2)
-
 print("y_train mean: " + str(y_train.mean()))
 print("y_test mean: " + str(y_test.mean()))
 
-print("Saving Model")
-model.save(db_path.rsplit('/', 1)[0]+"/model")
+num_folds = 5
+acc_per_fold = []
+loss_per_fold = []
+histories = []
+eval_scores = []
+kfold = KFold(n_splits = num_folds, shuffle = True, random_state=42)
+fold_no = 1
+for train, test in kfold.split(X_train):
+    print('------------------------------------------------------------------------')
+    print(f'Training for fold {fold_no} ...')
+
+    # create model
+    print("Creating model")
+    model = Sequential()
+    # in keras the first layer is a hidden layer too, so input dims is OK here
+    model.add(Dense(128, input_dim=128, kernel_initializer='normal', activation='relu')) #I know all input values will be positive at this point (SIFT)
+    model.add(Dense(64, kernel_initializer='normal', activation='relu'))
+    model.add(Dense(32, kernel_initializer='normal', activation='relu'))
+    model.add(Dense(16, kernel_initializer='normal', activation='relu'))
+    model.add(Dense(8, kernel_initializer='normal', activation='relu'))
+    model.add(Dense(1, kernel_initializer='normal', activation='sigmoid')) #TODO: relu might be more appropriate (?) here (since score can never be negative)
+
+    # Compile model
+    model.compile(optimizer='adam', loss='mse')
+
+    # train
+    print("Training.. on " + str(X_train[train].shape[0]) + " samples")
+    history = model.fit(X_train[train], y_train[train],
+                        validation_data=(X_train[test], y_train[test]),
+                        epochs=4, batch_size=6400,
+                        verbose=1)
+    histories.append(history)
+
+    score = model.evaluate(X_train[test], y_train[test], verbose=0, batch_size=6400)
+    print(f"Fold score (MSE): {score}")
+    eval_scores.append(score)
+
+    fold_no +=1
+
+# print("Evaluate Model..")
+# model.evaluate(X_test, y_test, verbose=2)
+#
+# print("y_train mean: " + str(y_train.mean()))
+# print("y_test mean: " + str(y_test.mean()))
+#
+# print("Saving Model")
+# model.save(db_path.rsplit('/', 1)[0]+"/model")
 
 # evaluation
 # estimator = KerasRegressor(build_fn=model, epochs=100, batch_size=20, verbose=0)
