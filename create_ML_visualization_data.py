27cacf3f6d3c3389f60df8abcc29ae1370681b0b
alex <ar2056@bath.ac.uk>
2021-03-07 16:49:33 +0000

    minor fixes

--- create_ML_visualization_data.py
+++ create_ML_visualization_data.py
@@ -7,6 +7,7 @@ import colmap
 from database import COLMAPDatabase
 from parameters import Parameters
 from query_image import get_image_id, get_keypoints_xy, get_queryDescriptors
+from point3D_loader import read_points3d_default, index_dict_reverse
 
 # This file will create the visualization data to view in threejs or any other viewer you create.
 # For images and pointclouds
@@ -19,8 +20,8 @@ from query_image import get_image_id, get_keypoints_xy, get_queryDescriptors
 # /home/alex/fullpipeline/colmap_data/Coop_data/slice1/ML_data/model/
 # /home/alex/fullpipeline/colmap_data/Coop_data/slice1/ML_data/visual_data/images/
 # /home/alex/fullpipeline/colmap_data/Coop_data/slice1/ML_data/visual_data/points/points3D_sorted_descending_heatmap_per_image.txt
-# /home/alex/fullpipeline/colmap_data/CMU_data/slice1/
-# oneliner: python3 create_ML_visualization_data.py /home/alex/fullpipeline/colmap_data/Coop_data/slice1/ML_data/test_db.db /home/alex/fullpipeline/colmap_data/Coop_data/slice1/ML_data/test_images/2020-06-22/ /home/alex/fullpipeline/colmap_data/Coop_data/slice1/ML_data/images_list.txt /home/alex/fullpipeline/colmap_data/Coop_data/slice1/ML_data/model/ /home/alex/fullpipeline/colmap_data/Coop_data/slice1/ML_data/visual_data/images/ /home/alex/fullpipeline/colmap_data/Coop_data/slice1/ML_data/visual_data/points/points3D_sorted_descending_heatmap_per_image.txt /home/alex/fullpipeline/colmap_data/CMU_data/slice1/
+# /home/alex/fullpipeline/colmap_data/Coop_data/slice1/
+# oneliner: python3 create_ML_visualization_data.py /home/alex/fullpipeline/colmap_data/Coop_data/slice1/ML_data/test_db.db /home/alex/fullpipeline/colmap_data/Coop_data/slice1/ML_data/test_images/2020-06-22/ /home/alex/fullpipeline/colmap_data/Coop_data/slice1/ML_data/images_list.txt /home/alex/fullpipeline/colmap_data/Coop_data/slice1/ML_data/model/ /home/alex/fullpipeline/colmap_data/Coop_data/slice1/ML_data/visual_data/images/ /home/alex/fullpipeline/colmap_data/Coop_data/slice1/ML_data/visual_data/points/points3D_sorted_descending_heatmap_per_image.txt /home/alex/fullpipeline/colmap_data/Coop_data/slice1/
 
 # test_db.db will be used to add data, so delete it before running this script
 test_db_path = sys.argv[1]
@@ -58,10 +59,14 @@ model = keras.models.load_model(model_path)
 
 # Points
 parameters = Parameters(base_path)
+# Load scores
 points3D_per_image_decay_scores = np.load(parameters.per_image_decay_matrix_path)
 points3D_per_image_decay_scores = points3D_per_image_decay_scores.sum(axis=0)
+# min-max normalization (not dividing by sum()) - this is done because in training I do the same, so it is a way to compare the model's output with these scores
+points3D_per_image_decay_scores = ( points3D_per_image_decay_scores - points3D_per_image_decay_scores.min() ) / ( points3D_per_image_decay_scores.max() - points3D_per_image_decay_scores.min() )
+# read points
 points3D = read_points3d_default(parameters.live_model_points3D_path) #Needs to be live model points, because of ids changing compared to base model ( because of colmap )
-points3D_id_index = index_dict_reverse(points3D)
+# load sift avgs for each point
 points3D_avg_sift_desc = np.load(parameters.avg_descs_live_path)
 
 total_dims = 133
@@ -69,7 +74,7 @@ points3D_xyz_score_sift = np.empty([0, total_dims])
 points3D_indexing = index_dict_reverse(points3D)
 for k,v in points3D.items():
     index = points3D_indexing[v.id]
-    score = points3D_scores[index]
+    score = points3D_per_image_decay_scores[index]
     avg_sift_vector = points3D_avg_sift_desc[index]
     import pdb
     pdb.set_trace()
