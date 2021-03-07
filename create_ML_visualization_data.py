013900f514daf33010774b535d6212e5dc061f99
alex <ar2056@bath.ac.uk>
2021-03-07 16:18:16 +0000

    added code to crete visualiztion data for ML

--- create_ML_visualization_data.py
+++ create_ML_visualization_data.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np
 from tensorflow import keras
 import sys
@@ -6,15 +7,30 @@ import colmap
 from database import COLMAPDatabase
 from query_image import get_image_id, get_keypoints_xy, get_queryDescriptors
 
-db_path = sys.argv[1]
+# This file will create the visualization data to view in threejs or any other viewer you create.
+# For images and pointclouds
+
+# Command:
+# python3 create_ML_visualization_data.py
+# /home/alex/fullpipeline/colmap_data/Coop_data/slice1/ML_data/test_db.db
+# /home/alex/fullpipeline/colmap_data/Coop_data/slice1/ML_data/test_images/2020-06-22/
+# /home/alex/fullpipeline/colmap_data/Coop_data/slice1/ML_data/images_list.txt
+# /home/alex/fullpipeline/colmap_data/Coop_data/slice1/ML_data/model/
+# /home/alex/fullpipeline/colmap_data/Coop_data/slice1/ML_data/results/
+# /home/alex/fullpipeline/colmap_data/CMU_data/slice1/
+# oneliner: python3 create_ML_visualization_data.py /home/alex/fullpipeline/colmap_data/Coop_data/slice1/ML_data/test_db.db /home/alex/fullpipeline/colmap_data/Coop_data/slice1/ML_data/test_images/2020-06-22/ /home/alex/fullpipeline/colmap_data/Coop_data/slice1/ML_data/images_list.txt /home/alex/fullpipeline/colmap_data/Coop_data/slice1/ML_data/model/ /home/alex/fullpipeline/colmap_data/Coop_data/slice1/ML_data/results/ /home/alex/fullpipeline/colmap_data/CMU_data/slice1/
+
+# test_db.db will be used to add data, so delete it before running this script
+test_db_path = sys.argv[1]
 images_dir = sys.argv[2]
 image_list_file = sys.argv[3]
 model_path = sys.argv[4]
 save_path = sys.argv[5]
+base_path = sys.argv[6] # example: "/home/alex/fullpipeline/colmap_data/CMU_data/slice1/" #trailing "/"
 
 # make sure the templates_ini/feature_extractions file are the same between Mobile-Pose.. and fullpipeline
-colmap.feature_extractor(db_path, images_dir, image_list_file, query=True)
-db = COLMAPDatabase.connect(db_path)
+colmap.feature_extractor(test_db_path, images_dir, image_list_file, query=True)
+db = COLMAPDatabase.connect(test_db_path)
 
 with open(image_list_file) as f:
     query_images = f.readlines()
@@ -22,16 +38,42 @@ query_images = [x.strip() for x in query_images]
 
 model = keras.models.load_model(model_path)
 
-for i in range(len(query_images)):
-    q_img = query_images[i]
-    image_id = get_image_id(db, q_img)
-    # keypoints data
-    keypoints_xy = get_keypoints_xy(db, image_id)
-    queryDescriptors = get_queryDescriptors(db, image_id)
+# Images
+# for i in range(len(query_images)):
+#     q_img = query_images[i]
+#     image_id = get_image_id(db, q_img)
+#     # keypoints data
+#     keypoints_xy = get_keypoints_xy(db, image_id)
+#     queryDescriptors = get_queryDescriptors(db, image_id)
+#
+#     predictions = model.predict(queryDescriptors)
+#
+#     data = np.concatenate((keypoints_xy, predictions), axis=1)
+#     data = data[data[:, 2].argsort()[::-1]]
+#
+#     np.savetxt(save_path + q_img.split(".")[0]+".txt", data)
 
-    predictions = model.predict(queryDescriptors)
+# Points
+parameters = Parameters(base_path)
+points3D_per_image_decay_scores = np.load(parameters.per_image_decay_matrix_path)
+points3D_per_image_decay_scores = points3D_per_image_decay_scores.sum(axis=0)
+points3D = read_points3d_default(parameters.live_model_points3D_path) #Needs to be live model points, because of ids changing compared to base model ( because of colmap )
+points3D_id_index = index_dict_reverse(points3D)
+points3D_avg_sift_desc = np.load(parameters.avg_descs_live_path)
 
-    data = np.concatenate((keypoints_xy, predictions), axis=1)
-    data = data[data[:, 2].argsort()[::-1]]
+total_dims = 133
+points3D_xyz_score_sift = np.empty([0, total_dims])
+points3D_indexing = index_dict_reverse(points3D)
+for k,v in points3D.items():
+    index = points3D_indexing[v.id]
+    score = points3D_scores[index]
+    avg_sift_vector = points3D_avg_sift_desc[index]
+    import pdb
+    pdb.set_trace()
+    row = np.array([v.xyz[0], v.xyz[1], v.xyz[2], score]).reshape([1,4])
+    row = np.c_[row, avg_sift_vector.reshape([1, 128])]
+    points3D_xyz_score_sift = np.r_[points3D_xyz_score_sift, row]
 
-    np.savetxt(save_path + q_img.split(".")[0]+".txt", data)
+points3D_xyz_score_sift = points3D_xyz_score_sift[points3D_xyz_score_sift[:,3].argsort()[::-1]]
+# sort points
+np.savetxt("colmap_data/Coop_data/slice1/points3D_sorted_descending_heatmap_per_image.txt", points3D_xyz_score_sift)
