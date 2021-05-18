d43d24abc83c86e9bf216039a3c4bbab64f74deb
alex <ar2056@bath.ac.uk>
2021-05-18 12:50:04 +0000

    debugging feature mathcer for ML

--- feature_matching_generator_ML.py
+++ feature_matching_generator_ML.py
@@ -2,6 +2,7 @@
 from itertools import chain
 import cv2
 import numpy as np
+import sys
 
 # creates 2d-3d matches data for ransac comparison
 def get_keypoints_xy(db, image_id):
@@ -96,10 +97,11 @@ def feature_matcher_wrapper(db, query_images, trainDescriptors, points3D_xyz, ra
 
     return matches
 
-def feature_matcher_wrapper_ml(db, query_images, trainDescriptors, points3D_xyz, ratio_test_val, verbose = False, points_scores_array=None, random_limit = -1):
+def feature_matcher_wrapper_ml(class_model, reg_model, db, query_images, trainDescriptors, points3D_xyz, ratio_test_val, verbose = False, points_scores_array=None, random_limit = -1):
     # create image_name <-> matches, dict - easier to work with
     matches = {}
     matches_sum = []
+    np.set_printoptions(threshold=sys.maxsize)
 
     #  go through all the test images and match their descs to the 3d points avg descs
     for i in range(len(query_images)):
@@ -111,44 +113,51 @@ def feature_matcher_wrapper_ml(db, query_images, trainDescriptors, points3D_xyz,
         keypoints_xy = get_keypoints_xy(db, image_id)
         queryDescriptors = get_queryDescriptors(db, image_id)
 
-        if(random_limit != -1):
-            # len(queryDescriptors) or len(keypoints_xy) - should return the number or rows and be the same.
-            random_idxs = np.random.choice(np.arange(len(queryDescriptors)), random_limit, replace=False)
-            keypoints_xy = keypoints_xy[random_idxs]
-            queryDescriptors = queryDescriptors[random_idxs]
+        queryDescriptors_class = class_model.predict(queryDescriptors)
+        queryDescriptors_reg = reg_model.predict(queryDescriptors)
 
-        matcher = cv2.BFMatcher()  # cv2.FlannBasedMatcher(Parameters.index_params, Parameters.search_params) # or cv.BFMatcher()
-        # Matching on trainDescriptors (remember these are the means of the 3D points)
-        temp_matches = matcher.knnMatch(queryDescriptors, trainDescriptors, k=2)
+        if(np.unique(queryDescriptors_class).shape[0] !=1):
+            import pdb
+            pdb.set_trace()
 
-        # output: idx1, idx2, lowes_distance (vectors of corresponding indexes in
-        # m the closest, n is the second closest
-        good_matches = []
-        for m, n in temp_matches: # TODO: maybe consider what you have at this point? and add it to the if condition ?
-            assert(m.distance <=  n.distance)
-            # trainIdx is from 0 to no of points 3D (since each point 3D has a desc), so you can use it as an index here
-            if (m.distance < ratio_test_val * n.distance): #and (score_m > score_n):
-                if(m.queryIdx >= keypoints_xy.shape[0]): #keypoints_xy.shape[0] always same as queryDescriptors.shape[0]
-                    raise Exception("m.queryIdx error!")
-                if (m.trainIdx >= points3D_xyz.shape[0]):
-                    raise Exception("m.trainIdx error!")
-                # idx1.append(m.queryIdx)
-                # idx2.append(m.trainIdx)
-                scores = []
-                xy2D = keypoints_xy[m.queryIdx, :].tolist()
-                xyz3D = points3D_xyz[m.trainIdx, :].tolist()
-
-                if (points_scores_array is not None):
-                    for points_scores in points_scores_array:
-                        scores.append(points_scores[0, m.trainIdx])
-                        scores.append(points_scores[0, n.trainIdx])
-
-                match_data = [xy2D, xyz3D, [m.distance, n.distance], scores]
-                match_data = list(chain(*match_data))
-                good_matches.append(match_data)
-
-        matches[query_image] = np.array(good_matches)
-        matches_sum.append(len(good_matches))
+        # if(random_limit != -1):
+        #     # len(queryDescriptors) or len(keypoints_xy) - should return the number or rows and be the same.
+        #     random_idxs = np.random.choice(np.arange(len(queryDescriptors)), random_limit, replace=False)
+        #     keypoints_xy = keypoints_xy[random_idxs]
+        #     queryDescriptors = queryDescriptors[random_idxs]
+        #
+        # matcher = cv2.BFMatcher()  # cv2.FlannBasedMatcher(Parameters.index_params, Parameters.search_params) # or cv.BFMatcher()
+        # # Matching on trainDescriptors (remember these are the means of the 3D points)
+        # temp_matches = matcher.knnMatch(queryDescriptors, trainDescriptors, k=2)
+        #
+        # # output: idx1, idx2, lowes_distance (vectors of corresponding indexes in
+        # # m the closest, n is the second closest
+        # good_matches = []
+        # for m, n in temp_matches: # TODO: maybe consider what you have at this point? and add it to the if condition ?
+        #     assert(m.distance <=  n.distance)
+        #     # trainIdx is from 0 to no of points 3D (since each point 3D has a desc), so you can use it as an index here
+        #     if (m.distance < ratio_test_val * n.distance): #and (score_m > score_n):
+        #         if(m.queryIdx >= keypoints_xy.shape[0]): #keypoints_xy.shape[0] always same as queryDescriptors.shape[0]
+        #             raise Exception("m.queryIdx error!")
+        #         if (m.trainIdx >= points3D_xyz.shape[0]):
+        #             raise Exception("m.trainIdx error!")
+        #         # idx1.append(m.queryIdx)
+        #         # idx2.append(m.trainIdx)
+        #         scores = []
+        #         xy2D = keypoints_xy[m.queryIdx, :].tolist()
+        #         xyz3D = points3D_xyz[m.trainIdx, :].tolist()
+        #
+        #         if (points_scores_array is not None):
+        #             for points_scores in points_scores_array:
+        #                 scores.append(points_scores[0, m.trainIdx])
+        #                 scores.append(points_scores[0, n.trainIdx])
+        #
+        #         match_data = [xy2D, xyz3D, [m.distance, n.distance], scores]
+        #         match_data = list(chain(*match_data))
+        #         good_matches.append(match_data)
+        #
+        # matches[query_image] = np.array(good_matches)
+        # matches_sum.append(len(good_matches))
 
     if(verbose):
         print()
