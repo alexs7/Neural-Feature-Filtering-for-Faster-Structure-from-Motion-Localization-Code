e3d9404ea01f418418848a729e4f56fa57ad3b69
alex <alexandrosrotsidis@icloud.com>
2021-05-21 17:42:27 +0300

    added minot changes in callback

--- custom_callback.py
+++ custom_callback.py
@@ -1,8 +1,9 @@
 from keras.callbacks import ModelCheckpoint, EarlyStopping
 
 def getModelCheckpointBinaryClassification(checkpoint_filepath):
-    return ModelCheckpoint(filepath=checkpoint_filepath, monitor='val_binary_accuracy', mode='max', save_best_only=True)
+    # https://stackoverflow.com/questions/61505749/tensorflowcan-save-best-model-only-with-val-acc-available-skipping
+    return ModelCheckpoint(filepath=checkpoint_filepath, monitor='val_binary_accuracy', mode='max', save_best_only=True, verbose=1)
 
 def getEarlyStoppingBinaryClassification():
     # why use loss here: https://machinelearningmastery.com/how-to-stop-training-deep-neural-networks-at-the-right-time-using-early-stopping/
-    return EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=200)
+    return EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=10)
