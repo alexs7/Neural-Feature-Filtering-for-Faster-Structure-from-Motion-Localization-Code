02a31083f15524e1a17bda4a97dc24ca9955fcb1
alex <alexandrosrotsidis@icloud.com>
2021-07-15 16:39:34 +0300

    minor refactoring

--- create_ML_training_data_CMU_all.py
+++ create_ML_training_data_CMU_all.py
@@ -32,9 +32,9 @@ def create_all_data(ml_db_path, slices):
         print("Doing slice: " + slice)
         base_path = slice #just for readability
         parameters = Parameters(base_path)
-        db = COLMAPDatabase.connect(parameters.live_db_path)
-        images = read_images_binary(parameters.live_model_images_path)
-        points3D = read_points3d_default(parameters.live_model_points3D_path)
+        db_live = COLMAPDatabase.connect(parameters.live_db_path)
+        live_model_images = read_images_binary(parameters.live_model_images_path)
+        live_model_points3D = read_points3d_default(parameters.live_model_points3D_path)
         # Getting the scores
         points3D_reliability_scores_matrix = np.load(parameters.per_image_decay_matrix_path)
         points3D_heatmap_vals_matrix = np.load(parameters.per_session_decay_matrix_path)
@@ -42,13 +42,13 @@ def create_all_data(ml_db_path, slices):
         points3D_reliability_scores = points3D_reliability_scores_matrix.sum(axis=0)
         points3D_heatmap_vals = points3D_heatmap_vals_matrix.sum(axis=0)
         points3D_visibility_vals = points3D_visibility_matrix.sum(axis=0)
-        points3D_id_index = index_dict_reverse(points3D)
+        points3D_id_index = index_dict_reverse(live_model_points3D)
 
         img_index = 0
         ml_db.execute("BEGIN")
-        for img_id , img_data in images.items():
-            print("Doing image " + str(img_index + 1) + "/" + str(len(images.items())), end="\r")
-            descs = get_image_decs(db, img_id)
+        for img_id , img_data in live_model_images.items():
+            print("Doing image " + str(img_index + 1) + "/" + str(len(live_model_images.items())), end="\r")
+            descs = get_image_decs(db_live, img_id)
             assert(img_data.xys.shape[0] == img_data.point3D_ids.shape[0] == descs.shape[0]) # just for my sanity
             for i in range(img_data.point3D_ids.shape[0]): # can loop through descs or img_data.xys - same thing
                 current_point3D_id = img_data.point3D_ids[i]
@@ -64,7 +64,7 @@ def create_all_data(ml_db_path, slices):
                     per_session_score = get_point3D_score(points3D_heatmap_vals, current_point3D_id, points3D_id_index)
                     visibility_score = get_point3D_score(points3D_visibility_vals, current_point3D_id, points3D_id_index)
                     matched = 1
-                    xyz = points3D[current_point3D_id].xyz  # np.float64
+                    xyz = live_model_points3D[current_point3D_id].xyz  # np.float64
 
                 desc = descs[i] # np.uint8
                 xy = img_data.xys[i] #np.float64, same as xyz
