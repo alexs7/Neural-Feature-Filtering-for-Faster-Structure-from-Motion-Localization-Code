d27a495ce6d672597fc4416965e2badbf760dd3b
alex <alexandrosrotsidis@icloud.com>
2021-07-16 12:30:15 +0300

    working script for creating the CMU all network

--- create_ML_training_data_CMU_all.py
+++ create_ML_training_data_CMU_all.py
@@ -1,17 +1,15 @@
 import os
 import pdb
 import sys
-from collections import defaultdict
 import numpy as np
 from database import COLMAPDatabase
 from parameters import Parameters
 from point3D_loader import read_points3d_default, index_dict_reverse
 from query_image import read_images_binary
-from sklearn.model_selection import train_test_split
 
 # refer to create_ML_training_data.py for a complete overview
 # this file is used to create train the network for the network(s) trained on all the CMU data
-# command to run: python3 create_ML_training_data.py colmap_data/CMU_data/slice3/ colmap_data/CMU_data/slice4/ colmap_data/CMU_data/slice6/ colmap_data/CMU_data/slice10/ colmap_data/CMU_data/slice11/
+# command to run: python3 create_ML_training_data_CMU_all.py colmap_data/CMU_data/slice3/ colmap_data/CMU_data/slice4/ colmap_data/CMU_data/slice6/ colmap_data/CMU_data/slice10/ colmap_data/CMU_data/slice11/
 
 def get_image_decs(db, image_id): #not to be confused with get_queryDescriptors() in feature_matching_generator.py - that one normalises descriptors.
     data = db.execute("SELECT data FROM descriptors WHERE image_id = " + "'" + str(image_id) + "'")
@@ -28,6 +26,7 @@ def get_point3D_score(points3D_scores, current_point3D_id, points3D_id_index):
 def create_all_data(ml_db_path, slices):
     ml_db = COLMAPDatabase.create_db_for_all_data(ml_db_path) #returns a connection
 
+    ml_db.execute("BEGIN")
     for slice in slices:
         print("Doing slice: " + slice)
         base_path = slice #just for readability
@@ -45,7 +44,6 @@ def create_all_data(ml_db_path, slices):
         points3D_id_index = index_dict_reverse(live_model_points3D)
 
         img_index = 0
-        ml_db.execute("BEGIN")
         for img_id , img_data in live_model_images.items():
             print("Doing image " + str(img_index + 1) + "/" + str(len(live_model_images.items())), end="\r")
             descs = get_image_decs(db_live, img_id)
@@ -75,44 +73,45 @@ def create_all_data(ml_db_path, slices):
                               (per_image_score,) + (per_session_score,) + (visibility_score,) +
                               (COLMAPDatabase.array_to_blob(xyz),) + (COLMAPDatabase.array_to_blob(xy),) + (matched,))
             img_index +=1
+        print("\n")
 
-        print()
-        print('Done!')
-        ml_db.commit()
+    print()
+    print('Done!')
+    ml_db.commit()
 
-        print("Generating Data Info...")
-        all_data = ml_db.execute("SELECT * FROM data ORDER BY image_id DESC").fetchall()
+    print("Generating Data Info for all CMU slices...")
+    all_data = ml_db.execute("SELECT * FROM data ORDER BY image_id DESC").fetchall()
 
-        all_sifts = (COLMAPDatabase.blob_to_array(row[2], np.uint8) for row in all_data)
-        all_sifts = np.array(list(all_sifts))
+    all_sifts = (COLMAPDatabase.blob_to_array(row[2], np.uint8) for row in all_data)
+    all_sifts = np.array(list(all_sifts))
 
-        per_image_scores = (row[3] for row in all_data)  # continuous values
-        per_image_scores = np.array(list(per_image_scores))
+    per_image_scores = (row[3] for row in all_data)  # continuous values
+    per_image_scores = np.array(list(per_image_scores))
 
-        per_session_scores = (row[4] for row in all_data)  # continuous values
-        per_session_scores = np.array(list(per_session_scores))
+    per_session_scores = (row[4] for row in all_data)  # continuous values
+    per_session_scores = np.array(list(per_session_scores))
 
-        visibility_scores = (row[5] for row in all_data)  # continuous values
-        visibility_scores = np.array(list(visibility_scores))
+    visibility_scores = (row[5] for row in all_data)  # continuous values
+    visibility_scores = np.array(list(visibility_scores))
 
-        all_classes = (row[8] for row in all_data)  # binary values
-        all_classes = np.array(list(all_classes))
+    all_classes = (row[8] for row in all_data)  # binary values
+    all_classes = np.array(list(all_classes))
 
-        print(" Total Training Size: " + str(all_sifts.shape[0]))
-        print(" per_image_scores mean: " + str(per_image_scores.mean()))
-        print(" per_session_scores mean: " + str(per_session_scores.mean()))
-        print(" visibility_scores mean: " + str(visibility_scores.mean()))
-        print(" per_image_scores std: " + str(per_image_scores.std()))
-        print(" per_session_scores std: " + str(per_session_scores.std()))
-        print(" visibility_scores std: " + str(visibility_scores.std()))
-        ratio = np.where(all_classes == 1)[0].shape[0] / np.where(all_classes == 0)[0].shape[0]
-        print(" Ratio of Positives to Negatives Classes: " + str(ratio))
-        print(" --------------------------->")
+    print(" Total Training Size: " + str(all_sifts.shape[0]))
+    print(" per_image_scores mean: " + str(per_image_scores.mean()))
+    print(" per_session_scores mean: " + str(per_session_scores.mean()))
+    print(" visibility_scores mean: " + str(visibility_scores.mean()))
+    print(" per_image_scores std: " + str(per_image_scores.std()))
+    print(" per_session_scores std: " + str(per_session_scores.std()))
+    print(" visibility_scores std: " + str(visibility_scores.std()))
+    ratio = np.where(all_classes == 1)[0].shape[0] / np.where(all_classes == 0)[0].shape[0]
+    print(" Ratio of Positives to Negatives Classes: " + str(ratio))
+    print(" --------------------------->")
 
 ml_db_dir = os.path.join("colmap_data/CMU_data/", "ML_data/")
 os.makedirs(ml_db_dir, exist_ok = True)
 ml_db_path = os.path.join(ml_db_dir, "ml_database_cmu_all.db")
 
 print("Creating all training data for all CMU slices..")
-slices = sys.argv
+slices = sys.argv[1:]
 create_all_data(ml_db_path , slices)
