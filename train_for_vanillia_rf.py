91a6061dcba5f08b054bfb5bbcc6258a4bb25093
alex <alexandrosrotsidis@icloud.com>
2022-10-20 21:00:58 +0300

    ml vanillia data comaprison models fix

--- train_for_vanillia_rf.py
+++ train_for_vanillia_rf.py
@@ -1,7 +1,6 @@
 # This file will load the training data for my original Neural Networks.
-# Train a vanillia RF classifier according
-# to run for all datasets in parallel:
-# python3 train_for_vanillia_rf.py colmap_data/CMU_data/slice3 25 25 & python3 train_for_vanillia_rf.py colmap_data/CMU_data/slice4 25 25 & python3 train_for_vanillia_rf.py colmap_data/CMU_data/slice6 25 25 & python3 train_for_vanillia_rf.py colmap_data/CMU_data/slice10 25 25 & python3 train_for_vanillia_rf.py colmap_data/CMU_data/slice11 25 25 & python3 train_for_vanillia_rf.py colmap_data/Coop_data/slice1 25 25 &
+# This will be used to compare the models from Predicting Matchability and Match no Match but with my own data this time.
+# This means tos use their models but my data. My data uses 3D points averages descriptors, not matches between images - remember ?
 
 import os
 import sys
@@ -10,23 +9,27 @@ from data import getClassificationData
 from joblib import dump
 
 base_path = sys.argv[1]
-n_estimators = int(sys.argv[2])
-max_depth = int(sys.argv[3])
 
 print("Base path: " + base_path)
 
+# TODO Continue from here
 output_path = os.path.join(base_path, "ml_models_vanillia_comparison_data")
 os.makedirs(output_path, exist_ok = True)
 ml_db = os.path.join(base_path, "ML_data/ml_database_all.db")
 
 print("Now train a vanillia RF -just to use as a baseline")
-rf_vanillia = RandomForestClassifier(n_estimators = n_estimators, max_depth = max_depth, random_state = 0)
 sift_vecs, classes = getClassificationData(ml_db)
 
-print("Training..")
-rf_vanillia.fit(sift_vecs, classes)
+# comparison models
+rf_pm = RandomForestClassifier(n_estimators = 25, max_depth = 25, random_state = 0)
+rf_mnm = RandomForestClassifier(n_estimators = 5, max_depth = 5, min_samples_split = 2, n_jobs=-1)
 
-print("Dumping model..")
-dump(rf_vanillia, os.path.join(output_path, f"rf_model_vanillia_{n_estimators}_{max_depth}.joblib"))
+print("Training PM..")
+rf_pm.fit(sift_vecs, classes)
+rf_mnm.fit(sift_vecs, classes)
+
+print("Dumping models..")
+dump(rf_pm, os.path.join(output_path, "rf_model_vanillia_pm.joblib"))
+dump(rf_mnm, os.path.join(output_path, "rf_model_vanillia_mnm.joblib"))
 
 print("Done!")
