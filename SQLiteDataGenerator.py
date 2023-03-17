74a6f0bf02c7c6e9c44556f21643b6768969e6c1
alex <alexandrosrotsidis@icloud.com>
2023-03-17 17:51:38 +0200

    my own generator works but very slow

---
+++ SQLiteDataGenerator.py
@@ -0,0 +1,90 @@
+import sqlite3
+import numpy as np
+from tensorflow.keras.utils import Sequence
+from database import COLMAPDatabase
+
+
+class SQLiteDataGenerator(Sequence):
+    def __init__(self, db_file, batch_size, input_shape, output_shape, val=False):
+        self.db_file = db_file
+        self.batch_size = batch_size
+        self.input_shape = input_shape
+        self.output_shape = output_shape
+        self.val = val
+
+        self.connection = sqlite3.connect(self.db_file, check_same_thread=False)
+        self.cursor = self.connection.cursor()
+
+        self.num_samples = self.get_num_samples()
+
+        # If this class is called to create a validation set then the number of samples
+        # is reduced to 30% of the total number of samples and the offset is set to 70%
+        # The offset will still work because we are querying from the whole database (all num_samples).
+        if(self.val):
+            self.val_offset = int(np.ceil(self.num_samples * 0.7))
+            self.num_samples = self.get_num_samples() - self.val_offset #30% of the data is used for validation
+        else:
+            self.val_offset = 0
+            self.num_samples = int(np.ceil(self.num_samples * 0.7)) #70% of the data is used for training
+
+    def __len__(self):
+        return int(np.ceil(self.num_samples / self.batch_size))
+
+    def __getitem__(self, idx):
+        batch_x = []
+        batch_y = []
+
+        start_idx = idx * self.batch_size
+        end_idx = (idx + 1) * self.batch_size
+
+        # At this point the data table is permanently randomised from the train_for_nf.py script
+        # If SQLiteDataGenerator was called to create training data then the offset is zero
+        # and does not have any effect here. The idx will take values from 0 to (int(0.7 * num_samples) / bath_size)-1
+        # If the SQLiteDataGenerator was called to create validation data then the offset is set to 70% of the total
+        # number of samples and the number of samples is reduced to 30% of the total number of samples.
+        # The idx will take values from 0 to (int(0.3 * num_samples) / bath_size)-1
+        start_idx += self.val_offset
+        end_idx += self.val_offset
+
+        assert end_idx - start_idx == self.batch_size, "Batch size is not equal to the number of samples"
+
+        query = f"SELECT sift, xy, blue, green, red, octave, angle, size, response, domOrientations, matched FROM data WHERE (base == 1 OR live == 1) LIMIT {start_idx}, {self.batch_size};"
+        self.cursor.execute(query)
+        results = self.cursor.fetchall()
+
+        batch = np.empty([self.batch_size, 139])
+        for i in range(len(results)):
+            row = results[i]
+            sift = COLMAPDatabase.blob_to_array(row[0], np.uint8)  # SIFT
+            xy = COLMAPDatabase.blob_to_array(row[1], np.float64)  # xy image
+            blue = row[2]  # blue
+            green = row[3]  # green
+            red = row[4]  # red
+            octave = row[5]  # octave
+            angle = row[6]  # angle
+            size = row[7]  # size
+            response = row[8]  # response
+            domOrientations = row[9]  # dominant orientations
+            matched = row[10]  # matched
+
+            batch[i,0:128] = sift
+            batch[i,128:130] = xy
+            batch[i,130] = blue
+            batch[i,131] = green
+            batch[i,132] = red
+            batch[i,133] = octave
+            batch[i,134] = angle
+            batch[i,135] = size
+            batch[i,136] = response
+            batch[i,137] = domOrientations
+            batch[i,138] = matched
+
+        batch_x = batch[:,0:138].astype(np.float32)
+        batch_y = batch[:,138].astype(np.float32)
+        return batch_x, batch_y
+
+    def get_num_samples(self):
+        query = "SELECT COUNT(*) FROM data WHERE (base == 1 OR live == 1);"
+        self.cursor.execute(query)
+        result = self.cursor.fetchone()
+        return result[0]
--- train_for_nf.py
+++ train_for_nf.py
@@ -1,10 +1,13 @@
 import datetime
 import os
 import numpy as np
+
+from SQLiteDataGenerator import SQLiteDataGenerator
 from custom_loss_nn import tweaked_loss
 from database import COLMAPDatabase
 from parameters import Parameters
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' #https://stackoverflow.com/questions/35911252/disable-tensorflow-debugging-information
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' #https://stackoverflow.com/questions/35911252/disable-tensorflow-debugging-information
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import Sequential
@@ -19,110 +22,64 @@ if tf.config.experimental.list_logical_devices('GPU'):
 else:
     print("No GPU found")
 
-def format_rows(sift, xy, blue, green, red, octave, angle, size, response, domOrientations, matched):
-    sift_tf = tf.io.decode_raw(sift, tf.uint8)  # SIFT
-    sift_tf = tf.reshape(sift_tf, [1,128])
-    xy_tf = tf.io.decode_raw(xy, np.float64) #xy
-    xy_tf = tf.reshape(xy_tf, [1,2])
+def run(base_path):
 
-    # Cast all tensors to float32, and reshape to 1x1 as needed
-    sift_tf = tf.cast(sift_tf, tf.float32)
-    xy_tf = tf.cast(xy_tf, tf.float32)
+    params = Parameters(base_path)
 
-    blue = tf.reshape(blue, [1,1])
-    green = tf.reshape(green, [1,1])
-    red = tf.reshape(red, [1,1])
-    octave = tf.reshape(octave, [1,1])
-    angle = tf.reshape(angle, [1,1])
-    size = tf.reshape(size, [1,1])
-    response = tf.reshape(response, [1,1])
-    domOrientations = tf.reshape(domOrientations, [1,1])
+    # print("Randomise table in db..")
+    # db = COLMAPDatabase.connect(db_path)
+    # db.execute("BEGIN")
+    # db.execute("CREATE TEMPORARY TABLE tmp_table AS SELECT * FROM data ORDER BY RANDOM();")
+    # db.execute("DELETE FROM data")
+    # db.execute("INSERT INTO table SELECT * FROM tmp_table")
+    # db.execute("DROP TABLE tmp_table")
+    # db.execute("COMMIT")
 
-    blue = tf.cast(blue, tf.float32)
-    green = tf.cast(green, tf.float32)
-    red = tf.cast(red, tf.float32)
-    octave = tf.cast(octave, tf.float32)
-    angle = tf.cast(angle, tf.float32)
-    size = tf.cast(size, tf.float32)
-    response = tf.cast(response, tf.float32)
-    domOrientations = tf.cast(domOrientations, tf.float32)
-
-    features = tf.concat([sift_tf, xy_tf, blue, green, red, octave, angle, size, response, domOrientations], axis=1)
-    features = tf.reshape(features, [138, 1])
-
-    label = tf.reshape(matched, [1, 1])
-    label = tf.cast(label, tf.float32)
-
-    return features, label
-
-def run(params):
-
-    base_path = params.base_path
-    batch_size = 4096
-    epochs = 1000
-    print("Batch_size: " + str(batch_size))
-    print("Epochs: " + str(epochs))
-
-    ml_path = os.path.join(base_path, "ML_data") #folder should exist from create_nf_training_data.py
+    ml_path = os.path.join(params.base_path, "ML_data") #folder should exist from create_nf_training_data.py
     os.makedirs(ml_path, exist_ok=True)
     nn_save_path = os.path.join(ml_path, "classification_model") #to store the binary model
     os.makedirs(nn_save_path, exist_ok=True)
     log_folder_name = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
     log_folder = os.path.join(ml_path, "tensorboard_logs", log_folder_name)
-    print(f"Tensorboard log folder: {log_folder}")
     callbacks = [TensorBoard(log_dir=log_folder, histogram_freq=1, write_graph=True, write_images=True, update_freq='epoch', profile_batch=2, embeddings_freq=1)]
+    print(f"Tensorboard log folder: {log_folder}")
+    batch_size = 4096
+    epochs = 1000
+    print("Batch_size: " + str(batch_size))
+    print("Epochs: " + str(epochs))
 
-    total_rows = 11940462
-    train_dataset = tf.data.experimental.SqlDataset("sqlite", params.ml_database_all_opencv_sift_path,
-                                              f"SELECT sift, xy, blue, green, red, octave, angle, size, response, domOrientations, matched FROM data WHERE (base == 1 OR live == 1) ORDER BY RANDOM() LIMIT {int(total_rows*0.7)}",
-                                              (tf.string, tf.string, tf.int32, tf.int32, tf.int32, tf.int32, tf.float64, tf.float64, tf.float64, tf.int32, tf.int32))
-    val_dataset = tf.data.experimental.SqlDataset("sqlite", params.ml_database_all_opencv_sift_path,
-                                              f"SELECT sift, xy, blue, green, red, octave, angle, size, response, domOrientations, matched FROM data  WHERE (base == 1 OR live == 1) ORDER BY RANDOM() LIMIT -1 OFFSET {int(total_rows*0.7)}",
-                                              (tf.string, tf.string, tf.int32, tf.int32, tf.int32, tf.int32, tf.float64, tf.float64, tf.float64, tf.int32, tf.int32))
-    # mapp function to help with formatting
-    train_dataset = train_dataset.map(format_rows)
-    val_dataset = val_dataset.map(format_rows)
-    train_dataset = train_dataset.batch(batch_size)
-    val_dataset = val_dataset.batch(batch_size)
-
-    # print("Splitting dataset")
-    # train_ds, val_ds = tf.keras.utils.split_dataset(formatted_dataset, left_size=0.7)
+    training_gen_data = SQLiteDataGenerator(params.ml_database_all_opencv_sift_path, batch_size, 138, 1)
+    validation_gen_data = SQLiteDataGenerator(params.ml_database_all_opencv_sift_path, batch_size, 138, 1, val=True)
 
     # Create model
     print("Creating model")
+    metrics = [
+        keras.metrics.TruePositives(name='tp'),
+        keras.metrics.FalsePositives(name='fp'),
+        keras.metrics.TrueNegatives(name='tn'),
+        keras.metrics.FalseNegatives(name='fn'),
+        keras.metrics.BinaryAccuracy(name='binary_accuracy'),
+        keras.metrics.Precision(name='precision'),
+        keras.metrics.Recall(name='recall'),
+        keras.metrics.AUC(name='auc'),
+        keras.metrics.AUC(name='prc', curve='PR'), # precision-recall curve
+    ]
 
-    strategy = tf.distribute.MirroredStrategy()
-    print('Number of devices: {}'.format(strategy.num_replicas_in_sync))
-
-    with strategy.scope():
-        metrics = [
-            keras.metrics.TruePositives(name='tp'),
-            keras.metrics.FalsePositives(name='fp'),
-            keras.metrics.TrueNegatives(name='tn'),
-            keras.metrics.FalseNegatives(name='fn'),
-            keras.metrics.BinaryAccuracy(name='binary_accuracy'),
-            keras.metrics.Precision(name='precision'),
-            keras.metrics.Recall(name='recall'),
-            keras.metrics.AUC(name='auc'),
-            keras.metrics.AUC(name='prc', curve='PR'), # precision-recall curve
-        ]
-
-        model = Sequential()
-        # in keras the first layer is a hidden layer too, so input dims is OK here
-        model.add(Dense(138, input_dim=138, activation='relu')) #Note: 'relu' here will be the same as 'linear' (default as all values are positive)
-        model.add(Dense(276, activation='relu'))
-        model.add(Dense(276, activation='relu'))
-        model.add(Dense(276, activation='relu'))
-        model.add(Dense(138, activation='relu'))
-        model.add(Dense(1, activation='sigmoid'))
-        # Compile model
-        opt = keras.optimizers.Adam(learning_rate=1e-4)
-        model.compile(optimizer=opt, loss=tweaked_loss, metrics=metrics)
-
+    model = Sequential()
+    # in keras the first layer is a hidden layer too, so input dims is OK here
+    model.add(Dense(138, input_dim=138, activation='relu')) #Note: 'relu' here will be the same as 'linear' (default as all values are positive)
+    model.add(Dense(276, activation='relu'))
+    model.add(Dense(276, activation='relu'))
+    model.add(Dense(276, activation='relu'))
+    model.add(Dense(138, activation='relu'))
+    model.add(Dense(1, activation='sigmoid'))
+    # Compile model
+    opt = keras.optimizers.Adam(learning_rate=1e-4)
+    model.compile(optimizer=opt, loss=tweaked_loss, metrics=metrics)
     model.summary()
 
     # NOTE: Before training you should use a baseline model
-    model.fit(train_dataset, validation_data=val_dataset, epochs=epochs, shuffle=True, verbose=1, callbacks=callbacks)
+    model.fit(training_gen_data, validation_data=validation_gen_data, epochs=epochs, shuffle=True, verbose=1, callbacks=callbacks)
 
     # Save model here
     print("Saving model..")
@@ -135,31 +92,28 @@ dataset = sys.argv[1]
 # To debug
 # debug = True
 # if(debug == True):
-# np.set_printoptions(threshold=sys.maxsize, precision=5, suppress=True)
-# tf.config.experimental_run_functions_eagerly(True)
-# tf.data.experimental.enable_debug_mode()
+#     # np.set_printoptions(threshold=sys.maxsize, precision=5, suppress=True)
+#     tf.config.experimental_run_functions_eagerly(True)
+#     tf.data.experimental.enable_debug_mode()
 
 if(dataset == "HGE" or dataset == "CAB" or dataset == "LIN"):
     base_path = os.path.join(root_path, "lamar", f"{dataset}_colmap_model")
     print("Base path: " + base_path)
-    print("Loading data..")
-    run(Parameters(base_path))
+    run(base_path)
 
-if(dataset == "CMU"):
-    if(len(sys.argv) > 4):
-        slices_names = [sys.argv[4]]
-    else: # do all slices
-        slices_names = ["slice2", "slice3", "slice4", "slice5", "slice6", "slice7", "slice8", "slice9", "slice10", "slice11", "slice12", "slice13", "slice14", "slice15",
-                        "slice16", "slice17", "slice18", "slice19", "slice20", "slice21", "slice22", "slice23", "slice24", "slice25"]
-    for slice_name in slices_names:
-        # overwrite paths
-        base_path = f"/media/iNicosiaData/engd_data/cmu/{slice_name}/exmaps_data/"
-        print("Base path: " + base_path)
-        print("Loading data..")
-        run(Parameters(base_path))
+# if(dataset == "CMU"):
+    # if(len(sys.argv) > 4):
+    #     slices_names = [sys.argv[4]]
+    # else: # do all slices
+    #     slices_names = ["slice2", "slice3", "slice4", "slice5", "slice6", "slice7", "slice8", "slice9", "slice10", "slice11", "slice12", "slice13", "slice14", "slice15",
+    #                     "slice16", "slice17", "slice18", "slice19", "slice20", "slice21", "slice22", "slice23", "slice24", "slice25"]
+    # for slice_name in slices_names:
+    #     # overwrite paths
+    #     base_path = f"/media/iNicosiaData/engd_data/cmu/{slice_name}/exmaps_data/"
+    #     print("Base path: " + base_path)
+    #     run(base_path)
 
 if(dataset == "RetailShop"):
     base_path = os.path.join(root_path, "retail_shop", "slice1")
     print("Base path: " + base_path)
-    print("Loading data..")
-    run(Parameters(base_path))
+    run(base_path)
